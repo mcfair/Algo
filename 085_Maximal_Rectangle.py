360c48a86bf61f336bdfab003b4b71131ece06ec
Mc Fair <mcfair18@gmail.com>
2018-07-18 13:36:55 -0700

    Update 085_Maximal_Rectangle.py

--- 085_Maximal_Rectangle.py
+++ 085_Maximal_Rectangle.py
@@ -27,3 +27,47 @@ class Solution(object):
                 
         return maxarea
         
+ 
+    def maximalRectangle2(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        #This solution is built on LC084's solution
+        #O(n*m) 112ms
+        if not matrix or not matrix[0]:
+            return 0
+        m, n = len(matrix), len(matrix[0])
+        
+        prev, maxa = [0]*n, 0
+      
+        for height in matrix:
+            height = [int(height[i])*(1+prev[i]) for i in range(n)]
+            prev = height
+            maxa = max(self.maxRecArea(height), maxa)
+            
+        return maxa
+    
+    
+    def maxRecArea(self, height):
+        #LeetCode084, O(n) 44ms
+        #High level idea is to find the max spread of each bar
+        #we can use a stack to store left index
+        #and right index is when we see a drop h[i] < h[i-1]
+        
+        height.append(-1)
+        stack, maxa = [-1], 0
+        
+        for i in range(len(height)):
+            while height[i] < height[stack[-1]]:
+                # "<" condition finds the right boundary "i-1" for all bars in the stack
+                # left boundary is the index in the stack 
+                h = height[stack.pop()]
+                l, r = stack[-1], i-1
+                w = r-l
+                maxa = max(maxa, h*w)           
+            else:
+                stack.append(i)
+        
+        height.pop()
+        return maxa
