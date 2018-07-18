6e939de7cfd68a2d35c5a15c64276978446ce24a
Mc Fair <mcfair18@gmail.com>
2018-07-18 12:29:36 -0700

    Update LC_084_Largest_Rectangle_in_Histogram.py

--- LC_084_Largest_Rectangle_in_Histogram.py
+++ LC_084_Largest_Rectangle_in_Histogram.py
@@ -1,10 +1,12 @@
+#Hard
+
 class Solution(object):
-    def largestRectangleArea(self, height):
+    def largestRectangleArea1(self, height):
         """
         :type heights: List[int]
         :rtype: int
         """
-    
+        #Method 1,  O(n) 44ms
         #Basic idea:
         #First step, find the max spread (w) of each bar with its height (height[i]) shrehold, 
         #Second step, take the max of w[i]*height[i] for all i
@@ -21,7 +23,7 @@ class Solution(object):
             l = i-1
             while height[i] <= height[l]:
                 L[i] = L[l]
-                l = L[i] -1 #skipping steps, instead of using l-=1
+                l = L[l] -1 #skipping steps, instead of using l-=1
              
         
         R = range(n+2) # right boundary
@@ -30,8 +32,29 @@ class Solution(object):
             r = i+1
             while height[i] <= height[r]:
                 R[i] = R[r]
-                r = R[i] +1 # skipping steps, instead of using r+=1
+                r = R[r] +1 # skipping steps, instead of using r+=1
             w = R[i]-L[i]+1
             maxa = max(maxa, w*height[i])  
                 
+        return maxa
+    
+    def largestRectangleArea2(self, height):
+        #high level idea is the same as method 1: find the max spread of each bar
+        #we can use a stack to store left index
+        
+        height.append(-1)
+        stack, maxa = [-1], 0
+        
+        for i in range(len(height)):
+            while height[i] < height[stack[-1]]:
+                # "<" condition finds the right boundary "i-1" for all bars in the stack
+                # left boundary is the index in the stack 
+                h = height[stack.pop()]
+                l, r = stack[-1], i-1
+                w = r-l
+                maxa = max(maxa, h*w)           
+            else:
+                stack.append(i)
+        
+        height.pop()
         return maxa
