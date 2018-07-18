d90cce846722af618e7c7102ffaa404bf0b22250
Mc Fair <mcfair18@gmail.com>
2018-07-18 13:45:52 -0700

    Update 085_Maximal_Rectangle.py

--- 085_Maximal_Rectangle.py
+++ 085_Maximal_Rectangle.py
@@ -1,4 +1,5 @@
 class Solution(object):
+    #Two methods: O(n^4) and O(n^2)
     def maximalRectangle1(self, matrix):
         #idea is to use cumsum to calculate any rectangle starting from (i,j) top-left to (k,l) bottom-right
         #O(n^4) TLE
@@ -27,13 +28,14 @@ class Solution(object):
                 
         return maxarea
         
- 
     def maximalRectangle2(self, matrix):
         """
         :type matrix: List[List[str]]
         :rtype: int
         """
         #This solution is built on LC084's solution
+        #Every row in the matrix is viewed as the ground with some buildings on it. 
+        #The building height is the count of consecutive 1s from that row to above rows.
         #O(n*m) 112ms
         if not matrix or not matrix[0]:
             return 0
@@ -52,8 +54,10 @@ class Solution(object):
     def maxRecArea(self, height):
         #LeetCode084, O(n) 44ms
         #High level idea is to find the max spread of each bar
-        #we can use a stack to store left index
-        #and right index is when we see a drop h[i] < h[i-1]
+        #Use a stack to store monotonically ascending indices
+        #keep adding i to stack, when h[i] >= h[stack[-1]]
+        #When we see a drop h[i] < h[stack[-1]] in the squence, we found a right boundary
+        #Left boundray of the spread is the 2nd item in the stack. First pop the 1st item to obtain h.
         
         height.append(-1)
         stack, maxa = [-1], 0
@@ -61,7 +65,7 @@ class Solution(object):
         for i in range(len(height)):
             while height[i] < height[stack[-1]]:
                 # "<" condition finds the right boundary "i-1" for all bars in the stack
-                # left boundary is the index in the stack 
+                # left boundary is the index on the stack top, after one pop() to get height
                 h = height[stack.pop()]
                 l, r = stack[-1], i-1
                 w = r-l
