6d97cccb344e97f22a62fc87e89b8ebd1e31f56e
Mc Fair <mcfair18@gmail.com>
2018-07-18 23:20:45 -0700

    Update 218_Skyline_Problem.py

--- 218_Skyline_Problem.py
+++ 218_Skyline_Problem.py
@@ -13,3 +13,39 @@ class Solution(object):
         return ret
 		
 		
+#O(n^2) TLE
+class Solution(object):
+    def getSkyline(self, buildings):
+        """
+        :type buildings: List[List[int]]
+        :rtype: List[List[int]]
+        
+        sweep line method (from huahua's video)
+        check all points on the vertical line, from high to low
+        if the highest is aleft endpoint: it's a key point
+        elif check the 2nd highest
+        """
+        events = [ event for b in buildings 
+                    for event in ( (b[0], -b[2],0),  (b[1],b[2],1))]
+        events.sort(key=lambda v: v)
+        #larger height goes earlier, when using negative height
+        q = [0] #heapq 
+        sky = [] #result
+        for v in events:
+            heapq.heapify(q)
+          
+            #enter the building
+            if v[2] ==0:
+                premax = q[0]
+                heapq.heappush(q, v[1])
+                if premax !=q[0]:
+                    sky.append([v[0], -v[1]])
+            #leave the building
+            else:
+                premax = q[0]
+                q.remove(-v[1])
+                heapq.heapify(q)
+                if premax!=q[0]:
+                    sky.append([v[0], -q[0]])
+           
+        return sky
