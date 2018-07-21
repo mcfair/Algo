2d5a730ed31fa26900ef421fc898cd7f31aafb80
Mc Fair <mcfair18@gmail.com>
2018-07-21 16:36:45 -0700

    Create 765_Couple_Holding_Hands.py

---
+++ 765_Couple_Holding_Hands.py
@@ -0,0 +1,27 @@
+
+
+
+
+#naive way O(n^2) time O(1) space, 20ms beats 100%
+class Solution(object):
+    def minSwapsCouples(self, row):
+        """
+        :type row: List[int]
+        :rtype: int
+        """
+        def couple(x):
+            return x-1 if x%2 else x+1
+        
+        nswap = 0
+        for i in range(0,len(row),2):
+            c = couple(row[i])
+            if row[i+1]!=c:
+                j = i+2
+                while j< len(row):
+                    if row[j]==c:
+                        nswap +=1
+                        row[j], row[i+1] = row[i+1],row[j]
+                        break
+                    j+=1
+        return nswap
+                
