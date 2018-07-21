38243bf216f66f764ad8949ec288569aeb6ba99e
Mc Fair <mcfair18@gmail.com>
2018-07-20 22:30:11 -0700

    Create 700_Search_BST.py

---
+++ 700_Search_BST.py
@@ -0,0 +1,16 @@
+#Very basic dfs, a building block for other BST problems
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        if val == root.val:
+            return root
+        elif val < root.val:
+            return self.searchBST(root.left, val)
+        elif val > root.val:
+            return self.searchBST(root.right, val)
