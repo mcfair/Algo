015a9e14118bc73871767901854f3d6d944893a8
Mc Fair <mcfair18@gmail.com>
2018-07-22 00:20:04 -0700

    Update 200_#_of_Islands.py

--- 200_#_of_Islands.py
+++ 200_#_of_Islands.py
@@ -6,26 +6,22 @@ class Solution(object):
         """
         if not grid or not grid[0]:
             return 0
-            
-        m , n , ans = len(grid), len(grid[0]), 0
         
-        def dfs(grid, i, j):
-            if not 0<=i<m or not 0<=j<n or grid[i][j] !='1':
+        m , n, ans = len(grid), len(grid[0]), 0
+        
+        
+        def dfs(i,j):
+            if not 0<=i<m or not 0<=j<n or grid[i][j]!='1':
                 return 
             grid[i][j] ='#'
-            dfs(grid, i+1, j)
-            dfs(grid, i-1, j)
-            dfs(grid, i, j+1)
-            dfs(grid, i, j-1)
-        
-        
+            dfs(i+1,j)
+            dfs(i-1,j)
+            dfs(i,j-1)
+            dfs(i,j+1)
         
         for i in range(m):
             for j in range(n):
                 if grid[i][j]=='1':
+                    dfs(i,j)
                     ans +=1
-                    dfs(grid, i,j)
-                    
-                    
         return ans
-                    
