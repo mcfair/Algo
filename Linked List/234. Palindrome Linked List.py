50223b758b98f07a21106c04deabc3fb9f8c4b7d
Mc Fair <mcfair18@gmail.com>
2018-07-23 16:40:21 -0700

    Update 234. Palindrome Linked List.py

--- Linked List/234. Palindrome Linked List.py
+++ Linked List/234. Palindrome Linked List.py
@@ -1,25 +1,38 @@
-
+#key is reversing the linked list while looking for middle point
+#reverse is classical 3-way swap: l, l.next, r = r, l, r.next
+#find middel point is classical slow/fast pointers
 class Solution(object):
     def isPalindrome(self, head):
         """
         :type head: ListNode
         :rtype: bool
         """
-        #find the middle while reverse the linked list
+        
         if not head or not head.next:
             return True
         
-        rev, slow, fast = None, head, head
+        left, slow, fast = None, head, head
         
+        #find the middle while reverse the linked list
         while fast and fast.next:
             fast = fast.next.next
-            rev, slow , rev.next  = slow, slow.next, rev 
+            left,  left.next, slow   = slow,  left , slow.next
         
+  
         #If the length of the linked list is odd
         if fast:
             slow = slow.next
-        #Compare the reversed first half with the second half.
-        while slow and rev.val == slow.val:
+            
+        #Compare the reversed left half ('left') with the right half ('slow').
+        while slow and left.val == slow.val:
             slow = slow.next
-            rev = rev.next
-        return slow==None
+            left = left.next
+        return left == None
+    
+    def printll(self, head):
+        ret = ''
+        while head:
+            ret += str(head.val) +'->'
+            head = head.next
+        ret += '#'
+        print ret
