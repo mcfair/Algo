8025b17e58f439866e7a5bf6bef7a059f881ea77
Mc Fair <mcfair18@gmail.com>
2018-07-23 11:10:05 -0700

    Create 408. Valid Word Abbreviation.py

---
+++ 408. Valid Word Abbreviation.py
@@ -0,0 +1,26 @@
+#Easy
+class Solution(object):
+    def validWordAbbreviation(self, word, abbr):
+        """
+        :type word: str
+        :type abbr: str
+        :rtype: bool
+        """
+
+        i = j = 0
+        m, n = len(word), len(abbr)
+        
+        while i < m and j < n:
+            if word[i] == abbr[j]:
+                i, j = i+1, j+1
+            elif abbr[j] == "0":
+                return False
+            elif abbr[j].isnumeric():
+                k = j
+                while k < n and abbr[k].isnumeric():
+                    k += 1
+                i += int(abbr[j:k])
+                j = k
+            else:
+                return False
+        return i == m and j == n
