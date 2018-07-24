52598054302fb245795250ef881402c4dd982f3c
Mc Fair <mcfair18@gmail.com>
2018-07-23 18:24:18 -0700

    Update 082. Remove Duplicates from Sorted List II.py

--- Linked List/082. Remove Duplicates from Sorted List II.py
+++ Linked List/082. Remove Duplicates from Sorted List II.py
@@ -1,4 +1,32 @@
- 
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        
+        dummy = ListNode('#');  # construct a dummy node
+        dummy.next = head 
+
+        pre = dummy           # set up pre and cur pointers
+        cur = head
+        while cur:
+            if cur.next and cur.val == cur.next.val:
+                # loop until cur point to the last duplicates
+                while cur and cur.next and cur.val == cur.next.val:
+                    cur = cur.next
+                pre.next = cur.next  # propose the next for pre
+                                     # this will be verified by next line
+            else:
+                pre = pre.next 
+                
+            cur = cur.next
+        return dummy.next
+        
+        
+#This solution doesn't assume it's sorted list.
+#It's actually as fast as Method 1 above
 class Solution(object):
     def deleteDuplicates(self, head):
         """
