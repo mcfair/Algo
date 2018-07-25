d05613d49177c48e7de7d8f518aa94d138186719
Mc Fair <mcfair18@gmail.com>
2018-07-24 17:08:17 -0700

    Create 315. Count of Smaller Numbers After Self.py

---
+++ Fenwick Tree/315. Count of Smaller Numbers After Self.py
@@ -0,0 +1,35 @@
+#BIT aka Fenwick Tree
+class BinaryIndexedTree(object):
+    def __init__(self, n):
+        self.sums = [0] * (n + 1)
+
+    def update(self, i, delta):
+        while i < len(self.sums):
+            self.sums[i] += delta
+            i += self._lowbit(i)
+
+    def query(self, i):
+        ret = 0
+        while i > 0:
+            ret += self.sums[i]
+            i -= self._lowbit(i)
+        return ret
+    
+    def _lowbit(self, i):
+        return i & -i
+    
+class Solution(object):
+    def countSmaller(self, nums):
+        #O(mlogm+n) m is the unique numbers in nums
+        #sort the unique numbers and map them to rank: 0 ~ m
+        rank = {v: i for i, v in enumerate(sorted(set(nums)))}
+        
+        tree  = BinaryIndexedTree(len(rank)) 
+        ret = [ ]
+        
+        #O(nlogm)
+        #scan from right to left
+        for x in nums[::-1]:
+            ret += tree.query(rank[x]),
+            tree.update(rank[x] + 1 , 1)
+        return ret[::-1]
