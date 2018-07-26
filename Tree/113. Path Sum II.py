045ea238313491c70f43fb8293231d1f7989e5ab
Mc Fair <mcfair18@gmail.com>
2018-07-25 21:13:59 -0700

    Update 113. Path Sum II.py

--- Tree/113. Path Sum II.py
+++ Tree/113. Path Sum II.py
@@ -1,3 +1,4 @@
+#DFS
 class Solution(object):
     def pathSum(self, root, sum):
         """
@@ -15,12 +16,25 @@ class Solution(object):
             if not root.left and not root.right and sum==root.val:
                 ret.append(path+[root.val])
                 return 
-            
-            if root.left:
-                dfs(root.left, sum-root.val, path+[root.val] )
-            if root.right:
-                dfs(root.right, sum-root.val, path+[root.val] )
+    
+             dfs(root.left, sum-root.val, path+[root.val] )
+        
+             dfs(root.right, sum-root.val, path+[root.val] )
          
           
         dfs(root, sum, [])
         return ret
+#BFS
+
+def pathSum(self, root, sum):
+    res = []
+    queue =  collections.deque([(root, sum, [])])
+    while queue:
+        node, sum, path = queue.popleft()
+        if node:
+            if node.val == sum and not node.left and not node.right:
+                res.append(path+[node.val])
+                continue
+            queue.append((node.left, sum-node.val, path+[node.val]))
+            queue.append((node.right, sum-node.val, path+[node.val]))
+    return res
