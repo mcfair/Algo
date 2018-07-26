56e1111ceaa5c7a6f5151e1b719fa37d2f883991
Mc Fair <mcfair18@gmail.com>
2018-07-25 17:18:45 -0700

    Update 666. Path Sum IV.py

--- Tree/666. Path Sum IV.py
+++ Tree/666. Path Sum IV.py
@@ -5,6 +5,49 @@ class Solution(object):
         :rtype: int
         """
         # ascending order tells you its in level order (BFS order)
+        # and in each layer it's sorted from left to right
+        # idea is to reconstruct the tree while adding layers by layers
+ 
+        tree = [[0]]
+        for x in nums:
+            lvl, pos, val, parent, isRightLeaf = self.parseDigits(x)
+            if lvl > len(tree) -1:  
+                tree.append(['']*2**lvl)
+            tree[lvl][pos] = val 
+            
+        prev = tree[0]
+  
+        for level in tree[1:]:
+            for i in range(0, len(level), 2):
+                #i/2 is parent index
+        
+                left , right = level[i], level[i+1]
+                if left is '' and   right is '':
+                    level[i+1] = prev[i/2]
+                if left is not '':
+                    level[i] +=prev[i/2]
+                if right is not '':
+                    level[i+1] +=prev[i/2]
+       
+            prev = level
+        return sum(filter(None, prev))
+                
+                
+    def parseDigits(self, x):
+        level, x = divmod(x, 100)
+        pos, val = divmod(x, 10)
+        isRightLeaf = (pos-1)%2
+        parent = (pos-1)//2
+        return level-1, pos-1, val, parent, isRightLeaf
+
+    
+class Solution(object):
+    def pathSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # ascending order tells you its in level order (BFS order)
         # idea is to reconstruct the tree using dict
  
         tree = collections.defaultdict(int)
