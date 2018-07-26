71ce3c49aef858cce306552ec5b98b966d3db717
Mc Fair <mcfair18@gmail.com>
2018-07-25 19:02:18 -0700

    Create 112. Path Sum.py

---
+++ Tree/112. Path Sum.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        
+        if not root:
+            return False
+
+        if not root.left and not root.right and root.val == sum:
+            return True
+        
+        sum -= root.val
+
+        return self.hasPathSum(root.left, sum) or self.hasPathSum(root.right, sum)
+    
+        
