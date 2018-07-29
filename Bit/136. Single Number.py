cc272abc8535aa11f4bba97bddfb286ecaf7f97d
Mc Fair <mcfair18@gmail.com>
2018-07-29 11:08:05 -0700

    Update and rename bit/136. Single Number.py to Bit/136. Single Number.py

--- Bit/136. Single Number.py
+++ Bit/136. Single Number.py
@@ -1,3 +1,6 @@
+#Naive solution is to just count the array, and find the single number. O(n) time with extra space O(k)
+
+#Solution based on bit manupilation is O(1) space.
 #Logic: XOR will return 1 only on two different bits. So if two numbers are the same, XOR will return 0. 
 #A ^ A = 0 and A ^ B ^ A = B.  Finally only one number left. 
 #This only works for even number of A, so it doesn't solve Leetcode137. Single Number II
