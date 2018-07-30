06763a9378bc9b1ee8f02bf1f137e568d0f50c19
Mc Fair <mcfair18@gmail.com>
2018-07-29 17:39:24 -0700

    Update 413. Arithmetic Slices.py

--- DP/413. Arithmetic Slices.py
+++ DP/413. Arithmetic Slices.py
@@ -1,4 +1,4 @@
-#MLE
+#MLE - use too much space
 class Solution(object):
     def numberOfArithmeticSlices(self, A):
         """
@@ -23,4 +23,30 @@ class Solution(object):
                     dp[i,j]=True
         
         return sum(dp.values())
+
+# from the MLE solution above, we can reduce space complexity
+# observe state transition function, current state only depends on one previous state j-1
+# so we only need two variables: prev and curr to store state transition info.
+class Solution(object):
+    def numberOfArithmeticSlices(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        #sound like a DP
+    
+        ans = 0
+        for lens in range(3, len(A)+1):
+            for i in range(0, len(A)+1 - lens):
+                j = i+lens -1
+                if lens==3:
+                    prev = A[j]+A[j-2]==A[j-1]*2
+                else:
+                    if prev and A[j]+A[j-2]==A[j-1]*2:
+                        curr = True
+                    else:
+                        curr = False
+                    prev = curr
+                ans += prev
+        return ans
     
