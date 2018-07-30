36d14d1fb900aa9eca4d3b697f8948dcc17b6506
Mc Fair <mcfair18@gmail.com>
2018-07-30 12:14:14 -0700

    Create 78. Subsets.py

---
+++ Combination/78. Subsets.py
@@ -0,0 +1,18 @@
+# Subsets I (no duplicates)        
+# it's basically to find all combinations
+# to do that,we need a dynamically updated list
+# similar to BFS code
+class Solution(object):
+    def subsets(self, nums):
+
+        result = [[]]
+        for num in nums:
+            result +=[[num] + pre for pre in result]
+        return result
+                      
+        """
+        print [[num] + pre for pre in result]
+        [[1]]
+        [[2], [2, 1]]
+        [[3], [3, 1], [3, 2], [3, 2, 1]]
+        """
