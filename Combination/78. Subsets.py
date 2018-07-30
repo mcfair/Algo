03d6df99bdf43b351480680902dff8f31e26ee72
Mc Fair <mcfair18@gmail.com>
2018-07-30 13:42:17 -0700

    Update 78. Subsets.py

--- Combination/78. Subsets.py
+++ Combination/78. Subsets.py
@@ -16,3 +16,17 @@ class Solution(object):
         [[2], [2, 1]]
         [[3], [3, 1], [3, 2], [3, 2, 1]]
         """
+
+#compare and contrast with solution to Leetcode 46 Permutations
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        ans = [[]]
+        for x in nums:
+            ans = [l[:i]+[x]+l[i:]
+                   for l in ans
+                   for i in xrange(len(l)+1)]
+        return ans
