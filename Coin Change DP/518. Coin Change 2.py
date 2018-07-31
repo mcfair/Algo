b4ccbe8c35ba62f1fccac4e319e98a60afd19ee3
Mc Fair <mcfair18@gmail.com>
2018-07-31 10:13:18 -0700

    Update 518. Coin Change 2.py

--- Coin Change DP/518. Coin Change 2.py
+++ Coin Change DP/518. Coin Change 2.py
@@ -8,9 +8,29 @@ class Solution(object):
         :rtype: int
         """ 
 
-        dp = [1]+[0 for _ in range(target)]
+        dp = [1]+[0]*target
         for x in nums:
             for b in range(x, 1+target):
                 dp[b] += dp[b-x]
                 
+        return dp[target]
+
+    
+    
+    def permutationSum(self, target, nums):
+        #permutation: put target loop outside
+        dp = [1] + [0] * target
+        for i in range(1, target + 1):
+            #dp[i] = sum([dp[i - c] for c in nums if i >= c])
+            for c in nums:
+                if i>=c:
+                    dp[i]+=dp[i-c]
+        return dp[target]
+    
+    def combinationSum(self, target, nums):
+        #combination: put target loop inside
+        dp = [1]+[0]*target
+        for c in nums:
+            for i in range(c, 1+target):
+                dp[i] += dp[i-c]
         return dp[target]
