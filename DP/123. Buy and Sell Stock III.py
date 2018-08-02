ef9a568463948db771b8bee7e41397289a870645
Mc Fair <mcfair18@gmail.com>
2018-08-02 16:00:30 -0700

    Create 123. Buy and Sell Stock III.py

---
+++ DP/123. Buy and Sell Stock III.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+       
+        if len(prices)<=1: return 0
+        
+        #O(n) -counting from left, find the max gain up to each day (not ending at each day)
+        left = [0]*len(prices)
+        curmin = prices[0]
+        for i in range(1, len(prices)):
+            curmin = min(curmin, prices[i])
+            left[i] = max(prices[i]-curmin, left[i-1])
+         
+        #O(n) -counting from right, find the max gain up to each day
+        right = [0]*len(prices)
+        curmax = prices[-1]
+        for i in range(len(prices)-2, -1, -1):
+            curmax = max(curmax, prices[i])
+            right[i] = max(curmax-prices[i], right[i+1])
+            
+        #varying i to find max(left[i]+right[i+1])    
+        max2t = 0
+        for i in range(len(prices)):
+            max2t = max(max2t, left[i] + right[i])
+        return max2t
+        
