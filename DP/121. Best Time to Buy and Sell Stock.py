bd575ff08ecc1530a4ea17256edd342b0f1774be
Mc Fair <mcfair18@gmail.com>
2018-08-02 14:03:51 -0700

    Create 121. Best Time to Buy and Sell Stock.py

---
+++ DP/121. Best Time to Buy and Sell Stock.py
@@ -0,0 +1,14 @@
+#keep track of the curmin - 'min price so far'
+#curgain = curprice - curmin
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        curmin = sys.maxint
+        curgain = 0
+        for p in prices:
+            curmin = min(curmin, p)
+            curgain = max(p-curmin, curgain)
+        return curgain
