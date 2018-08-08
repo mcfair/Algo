32af61fdc33a1759a1508509b92caab5aed9f3d0
Mc Fair <mcfair18@gmail.com>
2018-08-08 15:56:23 -0700

    Update 111_Min_Depth_of_Binary_Tree.py

--- Tree/111_Min_Depth_of_Binary_Tree.py
+++ Tree/111_Min_Depth_of_Binary_Tree.py
@@ -4,17 +4,17 @@ class Solution(Object):
         :type root: TreeNode
         :rtype: int
         """
-		#when a node is None, return 0
+	#when a node is None, return 0
         #when a node has no child, its depth = 1
         #when a node has single child, the depth is determined by this single child
         #when a node has two children, take the min of the two.
 		
         if not root: return 0
         if not root.left and not root.right: return 1
-		return 1 + min(self.minDepth(child) for child in (root.left, root.right) if child)
+	return 1 + min(self.minDepth(child) for child in (root.left, root.right) if child)
         
 		
-		#more verbosely
+	#more verbosely
         if not root: return 0
         if not root.left and not root.right: return 1
         if not root.left:
