a80fd57b04112659b8011474ee7809f5cb990549
Mc Fair <mcfair18@gmail.com>
2018-08-10 07:13:04 -0700

    Update 074. Search a 2D Matrix.py

--- 074. Search a 2D Matrix.py
+++ 074. Search a 2D Matrix.py
@@ -1,3 +1,26 @@
+  
+#O(log(mn))
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        if not matrix or target is None:
+            return False
+
+        rows, cols = len(matrix), len(matrix[0])
+        low, high = 0, rows * cols - 1
+        
+        while low <= high:
+            mid = (low + high) / 2
+            num = matrix[mid / cols][mid % cols]
+
+            if num == target:
+                return True
+            elif num < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        
+        return False
+
 #O(log(m) + log(n))
 class Solution(object):
     def searchMatrix(self, matrix, target):
