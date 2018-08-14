9231ab4378ad4d960eba31704f87e3d7b3769ed0
Mc Fair <mcfair18@gmail.com>
2018-08-13 23:02:52 -0700

    Update 745. Prefix and Suffix Search.py

--- Trie/745. Prefix and Suffix Search.py
+++ Trie/745. Prefix and Suffix Search.py
@@ -1,3 +1,42 @@
+#balanced solution for build and query
+#build O(nL^2)
+#query O(L)
+class Trie(object):
+    def __init__(self):
+        self.root = {}
+    def insert(self, word, weight):
+        cur = self.root
+        for c in word:
+            cur = cur.setdefault(c,{})
+            cur['$']= weight
+    def search(self, word):
+        cur = self.root
+        for c in word:
+            if c not in cur:
+                return -1
+            cur = cur[c]
+        return cur['$']
+    
+class WordFilter(object):
+    
+    def __init__(self, words):
+        """
+        :type words: List[str]
+        """
+        self.trie = Trie()
+        for weight, word in enumerate(words):
+            for i in range(len(word)+1):
+                self.trie.insert(word[i:]+'_'+word, weight)
+     
+    def f(self, prefix, suffix):
+        """
+        :type prefix: str
+        :type suffix: str
+        :rtype: int
+        """
+        return self.trie.search(suffix+'_'+prefix)
+    
+#solution for fast query    
 #O(nL^3) build hash table - 3 for loops O(nL^2) * concat string O(L)
 #query takes O(1)
 class WordFilter(object):
@@ -11,7 +50,6 @@ class WordFilter(object):
             for i in range(len(word)+1):
                 for j in range(len(word)+1):
                     self.dic[word[i:]+'_'+word[:j]] = weight
-        #for k in self.dic: print (k)
        
     def f(self, prefix, suffix):
         """
@@ -22,6 +60,4 @@ class WordFilter(object):
         return self.dic[suffix+'_'+prefix]
 
 
-# Your WordFilter object will be instantiated and called as such:
-# obj = WordFilter(words)
-# param_1 = obj.f(prefix,suffix)
+ )
