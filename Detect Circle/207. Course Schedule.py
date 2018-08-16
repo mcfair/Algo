2d136b96594ae3fccb5645c80b55ed746f6cc672
Mc Fair <mcfair18@gmail.com>
2018-08-16 13:14:55 -0700

    Create 207. Course Schedule.py

---
+++ Detect Circle/207. Course Schedule.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        if not prerequisites: 
+            return True
+        
+        g = collections.defaultdict(list)
+        for nxt, pre in prerequisites:
+            g[pre].append(nxt)
+        
+        visited = [0]*numCourses
+        
+        def hasCircle(i):
+            if visited[i]==1: return True
+            if visited[i]==2: return
+            
+            visited[i]=1
+            for j in g[i]:
+                if hasCircle(j):
+                    return True
+            visited[i]=2
+        
+        for i in range(numCourses):
+            if hasCircle(i):
+                return False
+        return True
