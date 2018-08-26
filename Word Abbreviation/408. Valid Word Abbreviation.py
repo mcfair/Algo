fb9c2ddf1c2a52170d0ea1fa86b7698acffa03eb
Mc Fair <mcfair18@gmail.com>
2018-08-25 18:44:59 -0700

    Update 408. Valid Word Abbreviation.py

--- Word Abbreviation/408. Valid Word Abbreviation.py
+++ Word Abbreviation/408. Valid Word Abbreviation.py
@@ -1,3 +1,25 @@
+#brutal force - geneate all abbrs - TLE
+class Solution(object):
+    def validWordAbbreviation(self, word, abbr):
+        """
+        :type word: str
+        :type abbr: str
+        :rtype: bool
+        """        
+        return abbr in self.genAbbr(word)
+    
+    def genAbbr(self, word):
+        def dfs(pos, count, abbr):
+            if pos==len(word):
+                ans.append(abbr+str(count) if count>0 else abbr)
+                return
+            dfs(pos+1, count+1, abbr)
+            dfs(pos+1, 0, abbr+str(count)+word[pos] if count>0 else abbr+word[pos])
+        
+        ans = []
+        dfs(0,0,'')
+        return ans
+
 #iterative
 class Solution(object):
     def validWordAbbreviation(self, word, abbr):
