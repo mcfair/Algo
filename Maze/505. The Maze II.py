65e164b9bf5b4fd4e5bb9d3b7150621001b755fa
Mc Fair <mcfair18@gmail.com>
2018-08-26 11:35:13 -0700

    Update 505. The Maze II.py

--- Maze/505. The Maze II.py
+++ Maze/505. The Maze II.py
@@ -1,3 +1,54 @@
 """
 Maze II: Find minimum steps
 """
+#use heap to ensure shortest dist, Dijkstra's algorithm
+class Solution(object):
+    def shortestDistance(self, maze, start, destination):
+        """
+        :type maze: List[List[int]]
+        :type start: List[int]
+        :type destination: List[int]
+        :rtype: int
+        """
+        Q = [[0]+start]
+        m, n = len(maze), len(maze[0])
+        dirs = ((1,0),(-1,0),(0,1),(0,-1))
+        
+        
+        while Q:
+            d, x, y = heapq.heappop(Q)
+            if [x,y]==destination:
+                return d
+            maze[x][y]='v'
+            for i, j in dirs:
+                row, col, newd = x, y, d
+                while 0<=row+i<m and 0<=col+j<n and maze[row+i][col+j]!=1:
+                    row, col, newd = row+i, col+j, newd+1
+                if maze[row][col]==0:
+                    heapq.heappush(Q, (newd,row,col))
+        return -1 
+      
+      
+#save all steps of all possible paths
+class Solution(object):
+    def shortestDistance(self, maze, start, destination):
+ 
+        Q = [[0]+start]
+        m, n = len(maze), len(maze[0])
+        dirs = ((1,0),(-1,0),(0,1),(0,-1))
+        
+        steps=[]
+        while Q:
+            d, x, y = heapq.heappop(Q)
+            if [x,y]==destination:
+                steps.append(d)
+                continue
+            maze[x][y]='v'
+            for i, j in dirs:
+                row, col, newd = x, y, d
+                while 0<=row+i<m and 0<=col+j<n and maze[row+i][col+j]!=1:
+                    row, col, newd = row+i, col+j, newd+1
+                if maze[row][col]==0:
+                    heapq.heappush(Q, (newd,row,col))
+                    
+        return -1 if not steps else min(steps)
