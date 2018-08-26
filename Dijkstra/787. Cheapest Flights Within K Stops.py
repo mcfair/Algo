dfb3d17fed9259a09463f421cc20e03f3c2d9c77
Mc Fair <mcfair18@gmail.com>
2018-08-26 15:21:07 -0700

    Update 787. Cheapest Flights Within K Stops.py

--- Dijkstra/787. Cheapest Flights Within K Stops.py
+++ Dijkstra/787. Cheapest Flights Within K Stops.py
@@ -1,25 +1,22 @@
 """
-:type n: int
-:type flights: List[List[int]]
-:type src: int
-:type dst: int
-:type K: int
-:rtype: int
+O(E+NlogN)
+This code is very powerful for followup questions, it returns totalPrice, numStops, and path from src to dst.
 """
-
 class Solution(object):
     def findCheapestPrice(self, n, flights, src, dst, K):
-
         g = collections.defaultdict(dict)
         for u, v, price in flights:
             g[u][v] = price
-        
-        heap = [(0, 0, src)] #(totalPrice, numStops, node)
+            
+        #tuple inside heap represents (totalPrice, numStops, node, path)
+        heap = [(0, 0, src, [src])] 
         while heap:
-            price, stops, i = heapq.heappop(heap)
+            price, stops, i, path = heapq.heappop(heap)
             if i == dst:
                 return price
             if stops<=K:
-                for j in g[i]:
-                    heapq.heappush(heap, (price+g[i][j], stops+1, j))
+                for j in g[i]:      
+                    if j not in path:
+                        heapq.heappush(heap, (price+g[i][j], stops+1, j, path+[j]))
+                      
         return -1
