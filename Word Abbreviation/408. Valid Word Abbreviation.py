869f4e035fe73c6a8c00f63999078ad1c3cc11ab
Mc Fair <mcfair18@gmail.com>
2018-08-25 19:04:54 -0700

    Update 408. Valid Word Abbreviation.py

--- Word Abbreviation/408. Valid Word Abbreviation.py
+++ Word Abbreviation/408. Valid Word Abbreviation.py
@@ -1,4 +1,54 @@
-#brutal force - geneate all abbrs - TLE
+
+#recursive
+class Solution(object):
+    def validWordAbbreviation(self, word, abbr):
+        """
+        :type word: str
+        :type abbr: str
+        :rtype: bool
+        """   
+        def check(i, j):
+            if i==len(word) and j==len(abbr):
+                return True
+            if i==len(word) or j==len(abbr):
+                return False
+            if word[i]==abbr[j]:
+                return check(i+1,j+1)
+            if abbr[j] in '123456789':
+                k = j+1
+                while k<len(abbr) and abbr[k] in '0123456789':
+                    k+=1
+                num = int(abbr[j:k])
+                return check(i+num, k)
+            return False
+                
+        return check(0,0)
+#iterative
+class Solution(object):
+    def validWordAbbreviation(self, word, abbr):
+        """
+        :type word: str
+        :type abbr: str
+        :rtype: bool
+        """
+
+        i = j = 0
+        m, n = len(word), len(abbr)
+        
+        while i < m and j < n:
+            if word[i] == abbr[j]:
+                i, j = i+1, j+1 
+            elif abbr[j] in '123456789':
+                k = j
+                while k < n and abbr[k].isnumeric():  # in '0123456789'
+                    k += 1
+                i += int(abbr[j:k])
+                j = k
+            else:
+                return False
+        return i == m and j == n
+    
+ #brutal force - geneate all abbrs - TLE
 class Solution(object):
     def validWordAbbreviation(self, word, abbr):
         """
@@ -19,30 +69,3 @@ class Solution(object):
         ans = []
         dfs(0,0,'')
         return ans
-
-#iterative
-class Solution(object):
-    def validWordAbbreviation(self, word, abbr):
-        """
-        :type word: str
-        :type abbr: str
-        :rtype: bool
-        """
-
-        i = j = 0
-        m, n = len(word), len(abbr)
-        
-        while i < m and j < n:
-            if word[i] == abbr[j]:
-                i, j = i+1, j+1
-            elif abbr[j] == "0":
-                return False
-            elif abbr[j].isnumeric():
-                k = j
-                while k < n and abbr[k].isnumeric():
-                    k += 1
-                i += int(abbr[j:k])
-                j = k
-            else:
-                return False
-        return i == m and j == n
