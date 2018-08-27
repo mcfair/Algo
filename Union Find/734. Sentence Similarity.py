1fccc62f52f1021c7111e71b87210571659513c4
Mc Fair <mcfair18@gmail.com>
2018-08-27 09:29:10 -0700

    Update 734. Sentence Similarity.py

--- Union Find/734. Sentence Similarity.py
+++ Union Find/734. Sentence Similarity.py
@@ -1,16 +1,16 @@
 
 #Note that the similarity relation is not transitive in the original question.
 #So this problem can't be solved by union-find, as it assumes transitivity.
-#Code is for practice.
+#Code actually solves 737. Sentence Similarity II
 
 class Solution(object):
-    def areSentencesSimilar(self, words1, words2, pairs):
+    def areSentencesSimilarTwo(self, words1, words2, pairs):
         """
         :type words1: List[str]
         :type words2: List[str]
         :type pairs: List[List[str]]
         :rtype: bool
-        """
+        """  
         if len(words1)!=len(words2):
             return False
         
@@ -18,13 +18,10 @@ class Solution(object):
         self.rank = {}
         
         for a, b in pairs:
-            print a,b
             self.union(a,b)
         
-        #for k,v in self.papa.items(): print k,v
-        
+     
         for w1,w2 in zip(words1, words2):
-            #print self.find(w1), self.find(w2)
             if self.find(w1)!=self.find(w2):
                 return False
         return True
