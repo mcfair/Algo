e0870b1573d6d5dad6b2bc0f97d11f1fbf2813b9
Mc Fair <mcfair18@gmail.com>
2018-08-28 10:14:29 -0700

    Update 218. The Skyline Problem.py

--- Heap/218. The Skyline Problem.py
+++ Heap/218. The Skyline Problem.py
@@ -54,15 +54,23 @@ class Solution(object):
 #48ms
 #https://leetcode.com/problems/the-skyline-problem/discuss/124354/Python-87ms-beats-100-with-explanation
 """
-Nearly the same method as '715. Range Module', regarding each building [l, r, h] as an interval [l, r). Use a list axis to record the positions of l and r. Use a list heights to record the height of the right-hand side of the position in axis with the same index. For example, heights[i] records the height between axis[i] and axis[i + 1], that is, the height on interval [axis[i], axis[i + 1]).
+Nearly the same method as '715. Range Module', regarding each building [l, r, h] as an interval [l, r). 
+Use a list axis to record the positions of l and r. Use a list heights to record the height of the right-hand side of the position in axis with the same index. 
+For example, heights[i] records the height between axis[i] and axis[i + 1], that is, the height on interval [axis[i], axis[i + 1]).
 
 First sort buildings by heights, and initialize axis and heights.
 Then for each building [l, r, h] we update the two lists following the rules:
 
 Find the indices of l and r in axis, called idl and idr. Then the range we are to update is [idl: idr].
-Since the current building is higher than or equal to anyone before it, the height of the right-hand side of l, that is heights[idl], should be h after updated. Meanwhile the height of the right-hand side of r wouldn't change after updated. That is, heights[idr] should be current heights[idr - 1] after updated.
+Since the current building is higher than or equal to anyone before it, the height of the right-hand side of l, 
+that is heights[idl], should be h after updated. Meanwhile the height of the right-hand side of r wouldn't change after updated. 
+That is, heights[idr] should be current heights[idr - 1] after updated.
+
 If h != heights[idl - 1], renew axis[idl: idr] = [l, r] and heights[idl: idr] = [h, heights[idr - 1]].
-Otherwise h == heights[idl - 1], it means there's a recorded building with height h whose left edge is less than l, hence we don't need to record l. If else idl == idr, it implies there's a recorded building with height h whose right edge is larger than r, then we don't renew anything. Otherwise we only record r and renew axis[idl: idr] = [r] and heights[idl: idr] = [heights[idr - 1]].
+Otherwise h == heights[idl - 1], it means there's a recorded building with height h whose left edge is less than l, 
+hence we don't need to record l. If else idl == idr, it implies there's a recorded building with height h whose right edge is larger than r, then we don't renew anything. 
+Otherwise we only record r and renew axis[idl: idr] = [r] and heights[idl: idr] = [heights[idr - 1]].
+
 After iteration, axis and heights record coordinates of all desired points in order.
 """
 class Solution(object):
