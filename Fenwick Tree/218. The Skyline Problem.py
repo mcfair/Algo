0d36fd268f52a9ce604f4d27103d211da467a5bd
Mc Fair <mcfair18@gmail.com>
2018-08-28 00:49:06 -0700

    Create 218. The Skyline Problem.py

---
+++ Fenwick Tree/218. The Skyline Problem.py
@@ -0,0 +1,45 @@
+class Solution(object):
+    def getSkyline(self, buildings):
+        """
+        :type buildings: List[List[int]]
+        :rtype: List[List[int]] 
+        """
+        points =[]
+        endsAt ={}
+        for L, R, H in buildings:
+            left, right = (L, 'left', -H), (R, 'right', -H) #'left' < 'right', don't use 'start' 'end'
+            points += [left,right]
+            endsAt[left] = right
+        
+        tree = BIT(len(points))
+        points.sort() #do we need to remove duplicates?
+        rank = {points[i]: i for i in range(len(points))} # sorted index
+        
+        ret = []
+        for p in points:
+            if p[1] == 'left':
+                q = endsAt[p]
+                tree.update(rank[q], -p[-1]) # end idx is exclusive
+            h = tree.query(rank[p]+1) # start idx is inclusive
+            
+            #not sure about this part!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            if not ret or ret[-1][1] != h:
+                if ret and ret[-1][0] == p[0]:
+                    ret[-1][1] = h
+                else:
+                    ret.append([p[0], h])
+        return ret
+    
+class BIT(object):
+    def __init__(self, m):
+        self.bit = [0] *(m+1)
+    def update(self, p, h):
+        while p > 0: # scope of h is towards left
+            self.bit[p] = max(self.bit[p], h)
+            p -= p & -p
+    def query(self,p):
+        ret = 0
+        while p <= len(self.bit): # check anything to the right that has a higher value
+            ret = max(ret, self.bit[p])
+            p += p & -p
+        return ret
