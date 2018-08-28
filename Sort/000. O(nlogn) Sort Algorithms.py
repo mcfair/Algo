c7ecd6f011055e9f44c470e23ab72c3913879d2c
Mc Fair <mcfair18@gmail.com>
2018-08-27 19:54:37 -0700

    Update 000. O(nlogn) Sort Algorithms.py

--- Sort/000. O(nlogn) Sort Algorithms.py
+++ Sort/000. O(nlogn) Sort Algorithms.py
@@ -19,7 +19,7 @@ def merge(left, right):
         return left or right
  
     result = []
-    i, j = 0, 0
+    i, j = 0, 0 #two pointer method
     while (len(result) < len(left) + len(right)):
         if left[i] < right[j]:
             result.append(left[i])
@@ -32,10 +32,7 @@ def merge(left, right):
             break
     return result
 
-#Heapsort is an in-place sorting algorithm with worst case and average complexity of O(nlogn).
-
-
-#Quick Sort is a Divide and Conquer algorithm. 
+#Quick Sort is also a Divide and Conquer algorithm. 
 #It picks an element as pivot and partitions the given array around the picked pivot.
 #There are different ways of picking pivot: always first item, always last item, random item, or median
 #O(nlogn) - Note the implementation below is not in-place.
@@ -55,5 +52,40 @@ def quickSort(arr):
     less = quickSort(less)
     more = quickSort(more)
     return less + pivotList + more
-    
 
+#Heap Sort is an in-place sorting algorithm with worst case and average complexity of O(nlogn).
+
+def heapify(arr, n, i):
+    """ To heapify subtree rooted at index i. n is size of heap"""
+    largest = i  # Initialize largest as root
+    l = 2 * i + 1     # left = 2*i + 1
+    r = 2 * i + 2     # right = 2*i + 2
+ 
+    # See if left child of root exists and is
+    # greater than root
+    if l < n and arr[i] < arr[l]:
+        largest = l
+ 
+    # See if right child of root exists and is
+    # greater than root
+    if r < n and arr[largest] < arr[r]:
+        largest = r
+ 
+    # Change root, if needed
+    if largest != i:
+        arr[i],arr[largest] = arr[largest],arr[i]  # swap
+ 
+        # Heapify the root.
+        heapify(arr, n, largest)
+        
+def heapSort(arr):
+    n = len(arr)
+ 
+    # Build a maxheap.
+    for i in range(n, -1, -1):
+        heapify(arr, n, i)
+ 
+    # One by one extract elements
+    for i in range(n-1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]   # swap
+        heapify(arr, i, 0)
