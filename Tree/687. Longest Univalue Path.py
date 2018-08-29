8107343aea8a732c8bab6270f7f83db0744c1cee
Mc Fair <mcfair18@gmail.com>
2018-08-28 22:37:57 -0700

    Create 687. Longest Univalue Path.py

---
+++ Tree/687. Longest Univalue Path.py
@@ -0,0 +1,25 @@
+ class Solution(object):
+    def longestUnivaluePath(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int 
+        """
+        if not root: return 0
+        
+        def dfs(root ):
+            l, r = 0, 0
+            
+            if root.left:
+                l=dfs(root.left )
+                l=l+1 if root.left.val==root.val else 0
+            if root.right:
+                r=dfs(root.right )
+                r=r+1 if root.right.val==root.val else 0
+                
+            self.ans = max(self.ans,r+l)
+            return max(l,r)
+        
+        self.ans = 0
+        dfs(root)
+        return self.ans
+        
