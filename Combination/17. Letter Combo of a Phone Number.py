b9e69b474b15453fbf79573a1c8dc2e33575741e
Mc Fair <mcfair18@gmail.com>
2018-08-29 07:19:59 -0700

    Update 17. Letter Combo of a Phone Number.py

--- Combination/17. Letter Combo of a Phone Number.py
+++ Combination/17. Letter Combo of a Phone Number.py
@@ -1,17 +1,24 @@
+#DFS backtracking
 class Solution(object):
     def letterCombinations(self, digits):
         """
         :type digits: str
         :rtype: List[str]
         """
-        if not digits: return []
         codec = {2:'abc', 3:'def', 4:'ghi',5:'jkl', 6:'mno',7:'pqrs',8:'tuv',9:'wxyz'}
-        
-        letters = [codec[int(i)] for i in digits]
-        
-        return reduce((lambda x, y: [i + j for i in x for j in y]), letters,['']) 
-        
-        
+  
+        def letterCombo(i):
+            if i == 0:
+                return []
+            if i == 1:
+                return list(codec[int(digits[0])])
+            prefix = self.letterCombinations(digits[:i-1])
+            additional = codec[int(digits[i-1])]
+            return [pre + c for pre in prefix for c in additional]  
+         
+        return letterCombo(len(digits))
+
+#Readable BFS
  class Solution(object):
     def letterCombinations(self, digits):
         if not digits: return []
@@ -26,4 +33,21 @@ class Solution(object):
             ans=tmp
        
         return ans
+
+#Reduce
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits: return []
+        codec = {2:'abc', 3:'def', 4:'ghi',5:'jkl', 6:'mno',7:'pqrs',8:'tuv',9:'wxyz'}
+        
+        letters = [codec[int(i)] for i in digits]
+        
+        return reduce((lambda x, y: [i + j for i in x for j in y]), letters,['']) 
+        
+        
+
                     
