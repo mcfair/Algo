3085e97e70cc77511b5c0f2e0873c5bca941dfbe
Mc Fair <mcfair18@gmail.com>
2018-08-28 23:13:41 -0700

    Update 572. Subtree of Another Tree.py

--- Tree/572. Subtree of Another Tree.py
+++ Tree/572. Subtree of Another Tree.py
@@ -1,3 +1,20 @@
+class Solution(object):
+    def isSubtree(self, s, t):
+        """
+        :type s: TreeNode
+        :type t: TreeNode
+        :rtype: bool
+        """ 
+        if not s: return False
+        def isSameTree(p, q):
+            if not p and not q: return True
+            if not p or not q: return False
+            if p.val != q.val: return False
+            return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
+        
+        return isSameTree(s, t) or self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
+        
+
 #Use a helper function "check"
 class Solution(object):
     def isSubtree(self, s, t):
