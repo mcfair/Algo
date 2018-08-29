9bca18ca52598a8ade66897e1fb2aaf73abc5206
Mc Fair <mcfair18@gmail.com>
2018-08-29 07:08:55 -0700

    Update 17. Letter Combo of a Phone Number.py

--- Combination/17. Letter Combo of a Phone Number.py
+++ Combination/17. Letter Combo of a Phone Number.py
@@ -16,11 +16,14 @@ class Solution(object):
     def letterCombinations(self, digits):
         if not digits: return []
         codec = {2:'abc', 3:'def', 4:'ghi',5:'jkl', 6:'mno',7:'pqrs',8:'tuv',9:'wxyz'}
-       
+        
         ans = ['']
         for d in list(digits):
-            #if d in'23456789':
-                ans = [prev+ch for prev in ans 
-                            for ch in codec[int(d)]]
+            tmp=[]
+            for prev in ans:
+                for ch in codec[int(d)]:
+                    tmp.append(prev+ch) 
+            ans=tmp
+       
         return ans
                     
