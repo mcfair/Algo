e1424eb1b81afc7a1bc39b23f639ec8b013dc254
Mc Fair <mcfair18@gmail.com>
2018-08-28 22:52:46 -0700

    Create 100. Same Tree.py

---
+++ Tree/100. Same Tree.py
@@ -0,0 +1,16 @@
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        
+        return False
