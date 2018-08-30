bd702cf3783718f959a4c419e03b2397a0a7b9ff
Mc Fair <mcfair18@gmail.com>
2018-08-30 13:28:57 -0700

    Update 678. Valid Parenthesis String.py

--- Parentheses/678. Valid Parenthesis String.py
+++ Parentheses/678. Valid Parenthesis String.py
@@ -1,4 +1,33 @@
+"""
+The first loop check sto see if we have enough '(' and '*' characters to balance out the ')' character. 
+If balance ==0 (var cnt in code below), it's already satisified, return True.
+If balance > 0, it means we have more than enough '('.
 
+The second loop checks to see if we have enough ')' and '*' to balance out the '(' character
+
+The first and second loop logic combine together and say we have enough '(', ')', '*' character 
+to balance out both the '(' and ')' parens.
+"""
+class Solution(object): 
+    def checkValidString(self, s):
+        cnt =0 
+        for c in s:
+            if c in '(*': cnt +=1
+            if c ==')':   cnt -=1
+            if cnt < 0:   return False
+        
+        if cnt ==0: 
+            return True
+        else:
+            cnt =0
+            
+        for c in s[::-1]:
+            if c in ')*': cnt +=1
+            if c =='(':   cnt -=1
+            if cnt < 0:   return False
+        
+        return True
+    
 #TLE -DFS
 class Solution(object): 
     def isValid(self,s):
