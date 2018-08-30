109d5c5e5f3f805cc108575da9e55825183ed2a6
Mc Fair <mcfair18@gmail.com>
2018-08-30 16:23:36 -0700

    Create 224. Basic Calculator.py

---
+++ Parentheses/224. Basic Calculator.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def calculate(self, s):
+        s = s.strip()+'+' #''.join(s.split()+['+'])
+        stack = [1]
+        lo = 0
+        ret = 0
+        for hi,c in enumerate(s):
+            if not c.isdigit():
+                if hi>lo:
+                    n = int(s[lo:hi])
+                    ret += reduce(operator.mul, stack) * n
+                if c == '+':
+                    stack[-1] = 1
+                elif c == '-':
+                    stack[-1] = -1
+                elif c == '(':
+                    stack.append(1)
+                elif c == ')':
+                    stack.pop()
+                lo = hi+1
+        return ret
