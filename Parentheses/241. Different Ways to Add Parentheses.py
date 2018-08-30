c0d5fe55df42840a069c2de5a70f78c580df5d9a
Mc Fair <mcfair18@gmail.com>
2018-08-30 11:19:43 -0700

    Update 241. Different Ways to Add Parentheses.py

--- Parentheses/241. Different Ways to Add Parentheses.py
+++ Parentheses/241. Different Ways to Add Parentheses.py
@@ -1,5 +1,8 @@
 #Divide and Conquer
 class Solution(object):
+    def __init__(self):
+        self.ops = {'+': operator.add, '-': operator.sub, '*': operator.mul} 
+    
     def diffWaysToCompute(self, input):
         """
         :type input: str
@@ -7,14 +10,13 @@ class Solution(object):
         """ 
         if input.isdigit():
             return [int(input)]
-        ops = {'+': operator.add, '-': operator.sub, '*': operator.mul} 
-    
+
         res = []
-        for i in xrange(len(input)):
-            if input[i] in "-+*":
+        for i, c in enumerate(input):
+            if c in "-+*":
                 res1 = self.diffWaysToCompute(input[:i])
                 res2 = self.diffWaysToCompute(input[i+1:])
                 for left in res1:
                     for right in res2:
-                        res.append(ops[input[i]](left,right))
+                        res.append(self.ops[c](left,right))
         return res
