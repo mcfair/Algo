0b0fd8e378967a6b4275bf1c9405533524cd4488
Mc Fair <mcfair18@gmail.com>
2018-08-30 09:45:30 -0700

    Update 22. Generate Parentheses.py

--- Parentheses/22. Generate Parentheses.py
+++ Parentheses/22. Generate Parentheses.py
@@ -1,1 +1,18 @@
-
+#dfs
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        def generate(p, left, right):
+            if left:         
+                generate(p + '(', left-1, right)
+            if right > left: 
+                generate(p + ')', left, right-1)
+            if not right:    
+                parens.append(p)
+                
+        parens = []
+        generate('', n, n)
+        return parens
