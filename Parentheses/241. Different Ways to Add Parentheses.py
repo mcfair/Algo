3cdc5c2b8e0144048094f7ac0fd156a54bef7527
Mc Fair <mcfair18@gmail.com>
2018-08-30 11:23:50 -0700

    Update 241. Different Ways to Add Parentheses.py

--- Parentheses/241. Different Ways to Add Parentheses.py
+++ Parentheses/241. Different Ways to Add Parentheses.py
@@ -1,4 +1,32 @@
-#Divide and Conquer
+#Divide and Conquer, with memoization beats 100% 20ms
+class Solution(object):
+    def __init__(self):
+        self.ops = {'+': operator.add, '-': operator.sub, '*': operator.mul} 
+        self.memo = {}
+    
+    def diffWaysToCompute(self, input):
+        """
+        :type input: str
+        :rtype: List[int]
+        """ 
+        if input in self.memo: 
+            return self.memo[input]
+        if input.isdigit():
+            return [int(input)]
+
+        res = []
+        for i, c in enumerate(input):
+            if c in "-+*":
+                res1 = self.diffWaysToCompute(input[:i])
+                res2 = self.diffWaysToCompute(input[i+1:])
+                for left in res1:
+                    for right in res2:
+                        res.append(self.ops[c](left,right))
+        self.memo[input] = res
+        return res
+    
+    
+#Divide and Conquer - no memo
 class Solution(object):
     def __init__(self):
         self.ops = {'+': operator.add, '-': operator.sub, '*': operator.mul} 
