efa79e54bd596dc513c6d6eea3a3615ce38cdb76
Mc Fair <mcfair18@gmail.com>
2018-08-30 14:58:17 -0700

    Update 32. Longest Valid Parentheses.py

--- Parentheses/32. Longest Valid Parentheses.py
+++ Parentheses/32. Longest Valid Parentheses.py
@@ -14,3 +14,26 @@ class Solution(object):
                 stack.append(i)
          
         return ans
+
+    
+  
+#less readability, but faster
+#use stack to store the value of "i-stack[-1]" in the method above
+class Solution:
+    def longestValidParentheses(self, s):
+        
+        stack = [0]
+        longest = 0
+        
+        for c in s:
+            if c == "(":
+                stack.append(0)
+            else:
+                if len(stack) > 1:
+                    val = stack.pop()
+                    stack[-1] += val + 2
+                    longest = max(longest, stack[-1])
+                else:
+                    stack = [0]
+
+        return longest
