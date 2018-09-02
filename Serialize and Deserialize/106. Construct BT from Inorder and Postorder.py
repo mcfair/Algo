e0cd3368109f00202c4f51a87561234834504bd8
Mc Fair <mcfair18@gmail.com>
2018-09-01 19:51:29 -0700

    Update 106. Construct BT from Inorder and Postorder.py

--- Serialize and Deserialize/106. Construct BT from Inorder and Postorder.py
+++ Serialize and Deserialize/106. Construct BT from Inorder and Postorder.py
@@ -1,1 +1,16 @@
 
+class Solution(object):
+    def buildTree(self, inorder, postorder):
+        """
+        :type inorder: List[int]
+        :type postorder: List[int]
+        :rtype: TreeNode
+        """       
+        if inorder:
+            ind = inorder.index(postorder.pop())
+            root = TreeNode(inorder[ind])
+            #must build from right to left, as we pop root from the end of postorder list
+            root.right = self.buildTree(inorder[ind+1:], postorder)
+            root.left = self.buildTree(inorder[:ind], postorder)
+            return root
+
