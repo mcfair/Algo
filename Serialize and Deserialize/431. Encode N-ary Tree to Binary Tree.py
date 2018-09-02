0d44c67dee6bb8fbe5fc9031a19abdf96c0c439e
Mc Fair <mcfair18@gmail.com>
2018-09-01 22:12:10 -0700

    Update 431. Encode N-ary Tree to Binary Tree.py

--- Serialize and Deserialize/431. Encode N-ary Tree to Binary Tree.py
+++ Serialize and Deserialize/431. Encode N-ary Tree to Binary Tree.py
@@ -1,1 +1,42 @@
+#Very Rare, nobody sees it in interview? 
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+        
+The left child of a binary node is the subtree encoding all the children of the corresponding n-ary node.
+The right child of a binary node is a chain of the binary root nodes encoding each sibling of the n-ary node.
+Hence the root node has no right binary child, because the root has no sibilings.
+"""
 
+class Codec:
+
+    def encode(self, root):
+        if not root:
+            return None
+
+        binary = TreeNode(root.val)                 # create a binary root
+        if not root.children:
+            return binary
+
+        binary.left = self.encode(root.children[0]) # left child of binary is the encoding of all n-ary children,
+        node = binary.left                          #     starting with the first child.
+        for child in root.children[1:]:             # other children of n-ary root are right child of previous child
+            node.right = self.encode(child)
+            node = node.right
+
+        return binary
+
+    def decode(self, data):
+        if not data:
+            return None
+
+        nary = Node(data.val, [])                   # create n-ary root
+        node = data.left                            # move to first child of n-ary root
+        while node:                                 # while more children of n-ary root
+            nary.children.append(self.decode(node)) # append to list
+            node = node.right                       # and move to next child
+            
+        return nary
