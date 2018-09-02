88648b8b77fb48251e222580b34a42c783c57108
Mc Fair <mcfair18@gmail.com>
2018-09-01 22:29:17 -0700

    Update 606. Construct String from BT.py

--- Serialize and Deserialize/606. Construct String from BT.py
+++ Serialize and Deserialize/606. Construct String from BT.py
@@ -1,1 +1,17 @@
 
+class Solution(object):
+    def tree2str(self, t):
+        """
+        :type t: TreeNode
+        :rtype: str
+        """
+        if not t:
+            return ""
+        res = ""
+        left = self.tree2str(t.left)
+        right = self.tree2str(t.right)
+        if left or right: #if not left and right, it will be "()(right)"
+            res += "(%s)" % left
+        if right:
+            res += "(%s)" % right
+        return str(t.val) + res
