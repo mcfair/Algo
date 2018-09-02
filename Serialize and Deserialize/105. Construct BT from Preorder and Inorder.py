62b96e65f1da9b2c31155d3f7f95a9e3e59602d0
Mc Fair <mcfair18@gmail.com>
2018-09-01 19:39:32 -0700

    Update 105. Construct BT from Preorder and Inorder.py

--- Serialize and Deserialize/105. Construct BT from Preorder and Inorder.py
+++ Serialize and Deserialize/105. Construct BT from Preorder and Inorder.py
@@ -1,1 +1,11 @@
 
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        if inorder:
+            #first item in preorder list is the root
+            ind = inorder.index(preorder.pop(0))
+            #build the tree
+            root = TreeNode(inorder[ind])
+            root.left = self.buildTree(preorder, inorder[0:ind])
+            root.right = self.buildTree(preorder, inorder[ind+1:])
+            return root
