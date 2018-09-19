954c3b9c3c928df7442794170b83a9dd5f843118
Mc Fair <mcfair18@gmail.com>
2018-09-19 13:08:10 -0700

    Update 734. Sentence Similarity.py

--- Union Find/734. Sentence Similarity.py
+++ Union Find/734. Sentence Similarity.py
@@ -1,48 +1,37 @@
 
 #Note that the similarity relation is not transitive in the original question.
-#So this problem can't be solved by union-find, as it assumes transitivity.
+#So this problem can't be solved by union-find, as it asserts transitivity.
 #Code actually solves 737. Sentence Similarity II
 
 class Solution(object):
-    def areSentencesSimilarTwo(self, words1, words2, pairs):
+    def areSentencesSimilar(self, words1, words2, pairs):
         """
         :type words1: List[str]
         :type words2: List[str]
         :type pairs: List[List[str]]
         :rtype: bool
-        """  
+        """
+        def isSimilar(w1,w2):
+            if w1==w2:
+                return True
+            if w1 in dic:
+                return w2 in dic[w1]
+            if w2 in dic:
+                return w1 in dic[w2]
+            return False
+        
         if len(words1)!=len(words2):
             return False
         
-        self.papa = {}
-        self.rank = {}
-        
+        dic = collections.defaultdict(set)
         for a, b in pairs:
-            self.union(a,b)
+            dic[a].add(b)
+            dic[b].add(a)
+        #for k,v in dic.items(): print k,v
         
-     
-        for w1,w2 in zip(words1, words2):
-            if self.find(w1)!=self.find(w2):
+
+        for w1,w2 in zip(words1,words2):
+            #print w1,w2, isSimilar(w1,w2)
+            if not isSimilar(w1,w2):
                 return False
         return True
-    
-    def find(self, x):
-        if x not in self.papa:
-            self.papa[x]=x
-            self.rank[x]=0
-            
-        while x!=self.papa[x]:
-            x, self.papa[x] = self.papa[x], self.papa[self.papa[x]]
-            
-        return x
-    
-    def union(self,x,y):
-        x, y = self.find(x), self.find(y)
-        if x!=y:
-            if self.rank[x] < self.rank[y]:
-                self.papa[x] = y
-            elif self.rank[x] > self.rank[y]:
-                self.papa[y] = x
-            else:
-                self.papa[y] = x
-                self.rank[x] +=1
