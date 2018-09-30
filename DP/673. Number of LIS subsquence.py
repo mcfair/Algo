d491e67c27dac0f1118abe7204193d7b422f8352
Mc Fair <mcfair18@gmail.com>
2018-09-30 12:45:35 -0700

    Update 673. Number of LIS subsquence.py

--- DP/673. Number of LIS subsquence.py
+++ DP/673. Number of LIS subsquence.py
@@ -1,6 +1,8 @@
-#calculate max len of LIS ending at i
-#use another array to keep track of how many occurence max len LIS for each i
-        
+"""
+Double DP 
+dp to store max len of LIS ending at i
+cnt to keep track of how many occurence max len LIS for each i
+"""
 class Solution(object):
     def findNumberOfLIS(self, nums):
         """
@@ -15,14 +17,10 @@ class Solution(object):
         for i in range(1, len(nums)):
             for j in range(i):
                 if nums[j] < nums[i]:
-                    if dp[j]+1> dp[i]: 
+                    if dp[j]+1 > dp[i]: 
                         dp[i] = dp[j] +1
                         cnt[i] = cnt[j]
                     elif dp[j]+1 == dp[i]: 
                         cnt[i] += cnt[j]
         
-        dic = collections.defaultdict(int)
-        for i in range(len(dp)):
-            dic[dp[i]] += cnt[i]
-       
-        return dic[max(dp)]
+        return sum(cnt[i] for i in range(len(nums)) if dp[i]==max(dp))
