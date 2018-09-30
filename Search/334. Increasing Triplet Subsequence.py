8831ebd0103dd5d39cec346f6f00172673877bee
Mc Fair <mcfair18@gmail.com>
2018-09-30 12:17:21 -0700

    Update 334. Increasing Triplet Subsequence.py

--- Search/334. Increasing Triplet Subsequence.py
+++ Search/334. Increasing Triplet Subsequence.py
@@ -1,5 +1,25 @@
-#O(n)
-#k is a constant, each search takes O(logk). O(log2)=O(log3)=1
+#O(n) to find an increasing triplet
+class Solution(object):
+    def increasingTriplet(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        l = m = float('inf')
+        
+        for r in nums:
+            if m < r:
+                return True
+            elif r < l:
+                l = r
+            elif l < r < m:
+                m = r
+            #print l, m, r
+        return False
+
+
+#O(nlogk) Generalize to k element sub-array
+#k is a constant, each search takes O(logk).  
 class Solution(object):
     def increasingTriplet(self, nums, k=3):
         """
