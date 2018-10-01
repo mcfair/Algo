61b133107dc7120e50a7da9ef3ca75ef2bd0afa8
Mc Fair <mcfair18@gmail.com>
2018-10-01 14:16:36 -0700

    Update 424. Longest Repeating Character Replacement.py

--- String/424. Longest Repeating Character Replacement.py
+++ String/424. Longest Repeating Character Replacement.py
@@ -1,1 +1,43 @@
-
+#standard two pointers
+"""
+Track two pointers and majority_count
+"""
+def characterReplacement(self, s, k):
+    """
+    :type s: str
+    :type k: int
+    :rtype: int
+    """  
+    count = collections.Counter()
+    start = result = 0
+    for end in range(len(s)):
+        count[s[end]] += 1
+        #majority_count = count.most_common(1)[0][1]
+        majority_count = max(majority_count, count[s[end]])  # 140ms with this line, compared to 400ms with line above.
+        if end - start + 1 > majority_count + k:
+            count[s[start]] -= 1
+            start += 1
+        result = max(result, end - start + 1)
+    return result
+  
+#method 2
+"""
+Keep track of curlen, end and majority_count
+"""
+def characterReplacement(self, s, k):
+    """
+    :type s: str
+    :type k: int
+    :rtype: int
+    """ 
+    majority = curlen = 0
+    count = collections.Counter()
+    for j in range(len(s)):
+        count[s[j]] += 1
+        majority = max(majority, count[s[j]])
+        if curlen - majority < k: 
+            curlen += 1
+        else: 
+            # start = j - curlen
+            count[s[j - curlen]] -= 1
+    return curlen
