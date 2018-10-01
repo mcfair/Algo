0a911e282c63a92a6e7d38ceeea56f75ee2b744f
Mc Fair <mcfair18@gmail.com>
2018-09-30 21:54:00 -0700

    Update 470. Implement Rand10() Using Rand7().py

--- Random/470. Implement Rand10() Using Rand7().py
+++ Random/470. Implement Rand10() Using Rand7().py
@@ -1,5 +1,9 @@
-#straight-forward solution, use rand7 to represent rand49
-#return rand40%10 + 1
+"""
+straight-forward solution, 
+use rand7 to represent range 1~49
+if num <=40, return num%10+1
+The average call of rand7 is 2 * 49 / 40 = 2.45
+"""
 
 class Solution(object):
     def rand10(self):
@@ -7,3 +11,24 @@ class Solution(object):
         while res > 40:
             res = (rand7()-1)*7 + rand7()
         return res%10+1
+
+   
+#most optimized way
+
+class Solution(object):
+    def __init__(self):
+        self.cache = []
+        
+    def rand10(self):
+        while not self.cache: 
+            self.generate()
+        return self.cache.pop()
+
+    def generate(self):
+        n = 19  
+        curnum = sum((rand7() - 1) * (7**i) for i in range(n))
+        rang = 7 ** n
+        while curnum < rang / 10 * 10:
+            self.cache.append(curnum % 10 + 1)
+            curnum /= 10
+            rang /= 10
