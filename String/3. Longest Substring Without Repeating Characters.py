1c3c87642b8b0f489a0a0daf9e0d49a1bc11c57f
Mc Fair <mcfair18@gmail.com>
2018-10-01 12:36:36 -0700

    Update 3. Longest Substring Without Repeating Characters.py

--- String/3. Longest Substring Without Repeating Characters.py
+++ String/3. Longest Substring Without Repeating Characters.py
@@ -1,1 +1,23 @@
+ 
+def lengthOfLongestSubstring(self, s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    if not s: return 0
+
+    ans, i = 1, 0
+    s = s +'#'
+    hashset = set(['#'])
+    for j, c in enumerate(s):
+        if c in hashset:
+            ans = max(ans, j-i)
+            while i<j and c in hashset and s[i] in hashset:
+                hashset.remove(s[i])
+                i +=1
+        hashset.add(c)
+
+    return  max(ans, j-i)          
+
+
 
