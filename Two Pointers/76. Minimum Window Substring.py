1bb76b6300ab41156e005a306308255a27d0edc0
Mc Fair <mcfair18@gmail.com>
2018-10-01 11:06:24 -0700

    Update 76. Minimum Window Substring.py

--- Two Pointers/76. Minimum Window Substring.py
+++ Two Pointers/76. Minimum Window Substring.py
@@ -1,1 +1,25 @@
-#sliding window
+#classical sliding window
+
+def minWindow(s, t):
+    needed = collections.Counter(t)          #hash table to store char frequency
+    missing = len(t)                         #total number of chars we need to find
+    start, end = 0, 0                        #(start, end) pointers to store min window
+    i = 0                                    #(i,j) is the moving window
+    for j, char in enumerate(s, 1):          #index j from 1, move j to find matches
+        if needed[char] > 0:
+            missing -= 1
+        needed[char] -= 1                    #needed can be negative, which represents redundency (more than needed)
+        
+        if missing == 0:                     #all chars matched, let's move i now.
+            while i < j and needed[s[i]] < 0:#remove redundent chars to find the i to form the min window
+                needed[s[i]] += 1
+                i += 1
+                
+            if end == 0 or j-i < end-start:  #update window
+                start, end = i, j
+                
+            i += 1                           #move i to i+1 for next window
+            needed[s[i]] += 1                #make sure the first appearing char satisfies need[char]>0
+            missing += 1                     #we missed this first char, so add missing by 1
+      
+     return s[start:end]
