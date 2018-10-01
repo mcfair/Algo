bff3af6a66f813685067a7ab383139fb2ff1df7d
Mc Fair <mcfair18@gmail.com>
2018-10-01 10:59:20 -0700

    Update 76. Minimum Window Substring.py

--- String/76. Minimum Window Substring.py
+++ String/76. Minimum Window Substring.py
@@ -1,18 +1,19 @@
 
 def minWindow(s, t):
-    needed = collections.Counter(t)            #hash table to store char frequency
-    missing = len(t)                         #total number of chars we care
-    start, end = 0, 0
-    i = 0
-    for j, char in enumerate(s, 1):          #index j from 1
+    needed = collections.Counter(t)          #hash table to store char frequency
+    missing = len(t)                         #total number of chars we need to find
+    start, end = 0, 0                        #(start, end) pointers to store min window
+    i = 0                                    #(i,j) is the moving window
+    for j, char in enumerate(s, 1):          #index j from 1, move j to find matches
         if needed[char] > 0:
             missing -= 1
-        needed[char] -= 1
-        if missing == 0:                     #match all chars
-            while i < j and needed[s[i]] < 0:  #remove chars to find the real start
+        needed[char] -= 1                    #needed can be negative, which represents redundency (more than needed)
+        
+        if missing == 0:                     #all chars matched, let's move i now.
+            while i < j and needed[s[i]] < 0:  #remove redundent chars to find the i to form the min window
                 needed[s[i]] += 1
                 i += 1
-            needed[s[i]] += 1                  #make sure the first appearing char satisfies need[char]>0
+            needed[s[i]] += 1                #make sure the first appearing char satisfies need[char]>0
             missing += 1                     #we missed this first char, so add missing by 1
             if end == 0 or j-i < end-start:  #update window
                 start, end = i, j
