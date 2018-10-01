0b08c5b3cc0e1ea6208628173cb2857b3aed503d
Mc Fair <mcfair18@gmail.com>
2018-10-01 13:07:41 -0700

    Update 3. Longest Substring Without Repeating Characters.py

--- String/3. Longest Substring Without Repeating Characters.py
+++ String/3. Longest Substring Without Repeating Characters.py
@@ -1,4 +1,4 @@
- 
+#use set
 def lengthOfLongestSubstring(self, s):
     """
     :type s: str
@@ -20,4 +20,18 @@ def lengthOfLongestSubstring(self, s):
     return  max(ans, j-i)          
 
 
+ 
+ #use dict to store index, allow i to directly jump to new eligible start
+ def lengthOfLongestSubstring(self, s):
+     if not s: return 0
+     ans = i = 0
+     occur = {} 
 
+     for j, c in enumerate(s):
+         if c in occur and i <= occur[c]:
+             ans = max(ans, j-i)
+             i = occur[c] + 1  
+             #directly skip to the next window to avoid the while loop in the first method
+         occur[c]=j
+
+     return max(ans, j-i+1)
