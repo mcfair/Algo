4be264a76d25de8aba001c69802696da4d4e4f26
Mc Fair <mcfair18@gmail.com>
2018-10-01 13:36:38 -0700

    Update 340. Longest Substring with At Most K Distinct Characters.py

--- String/340. Longest Substring with At Most K Distinct Characters.py
+++ String/340. Longest Substring with At Most K Distinct Characters.py
@@ -1,1 +1,23 @@
-
+ class Solution(object):
+    def lengthOfLongestSubstringKDistinct(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """  
+        if k==0 or not s: return 0
+       
+        dic = {}
+        i = ans = 0
+        for j, c in enumerate(s):
+            if len(dic) == k  and c not in dic:
+                ans = max(ans, j-i)
+                while i<j and len(dic)==k:
+                    if dic[s[i]]==i:
+                        del dic[s[i]]
+                    i+=1
+                
+            dic[c] = j
+               
+       
+        return max(ans, j-i+1)
