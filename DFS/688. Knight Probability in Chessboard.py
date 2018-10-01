5f7e4b753e9175cc462c4c670e3380ece3018460
Mc Fair <mcfair18@gmail.com>
2018-09-30 22:09:44 -0700

    Create 688. Knight Probability in Chessboard.py

---
+++ DFS/688. Knight Probability in Chessboard.py
@@ -0,0 +1,22 @@
+"""
+P(in chessboard) = (P(move1) + P(move2) + ... + P(move8))/8
+This is a chained equation, use DFS and memorizatin should work
+"""
+
+class Solution: 
+    def knightProbability(self, N, K, r, c):
+        memo = {}
+        moves = ((-1, -2), (-2, -1), (-2, 1), (-1, 2), (1, 2), (2, 1), (2, -1), (1, -2))
+        def dfs(k, x, y, P):
+            p = 0
+            if 0 <= x < N and 0 <= y < N: #a valid position on the board
+                if k < K:
+                    for dx, dy in moves:
+                        x_next, y_next = x + dx, y + dy
+                        if (x_next, y_next, k+1) not in memo:
+                            memo[(x_next, y_next, k+1)] = dfs(k+1, x_next, y_next, P/8)
+                        p += memo[(x_next, y_next, k+1)]
+                else: # k==K, this is the last move
+                    p = P
+            return p
+        return dfs(0, r, c, P=1.0)
