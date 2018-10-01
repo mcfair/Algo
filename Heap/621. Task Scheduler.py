5f575ef520fe4713160dc7d2efe56a0be3bc361e
Mc Fair <mcfair18@gmail.com>
2018-10-01 07:05:47 -0700

    Create 621. Task Scheduler.py

---
+++ Heap/621. Task Scheduler.py
@@ -0,0 +1,23 @@
+#Iterative way - O(nlogn)
+
+from collections import Counter
+ 
+def leastInterval(self,tasks,n):
+
+    q = sorted(Counter(tasks).values())
+
+    maxInterval = q.pop()-1
+    idleSlotes = n*maxInterval
+    while q:
+        idleSlotes -= min(q.pop(), maxInterval)
+
+    return idleSlotes+len(tasks) if idleSlotes > 0 else len(tasks)    
+
+        
+#Math way - O(n) 
+
+def leastInterval(self, tasks, N):
+    task_counts = collections.Counter(tasks).values()
+    M = max(task_counts)
+    Msame = task_counts.count(M)
+    return max(len(tasks), (M - 1) * (N + 1) + Msame)
