7101953bbd2dbe72dc74c8a5e553b6601c239235
Mc Fair <mcfair18@gmail.com>
2018-10-02 10:46:55 -0700

    Update 405. Integer Number to Hexadecimal.py

--- Bit/405. Integer Number to Hexadecimal.py
+++ Bit/405. Integer Number to Hexadecimal.py
@@ -1,12 +1,11 @@
+
 def toHex(self, num):
-    if not num: return "0"
-    mp = '0123456789abcdef'
-    res = ''
-    for i in xrange(8):
-        n = num & 15
-        c = mp[n]
-        res = c + res
-        num >>= 4
-        if num == 0:
-            break
+
+    if num==0: return '0'
+    table, res = '0123456789abcdef', ''
+    i = 0
+    while num and i<8:
+        res = table[num & 0xF] + res #take last for bits and add to the left
+        num = num>>4
+        i +=1
     return res
