d6b6861e0143cd0d5fbfa9079ccf85f0b23c363f
Mc Fair <mcfair18@gmail.com>
2018-10-02 11:37:28 -0700

    Update 693. Binary Number with Alternating Bits.py

--- Bit/693. Binary Number with Alternating Bits.py
+++ Bit/693. Binary Number with Alternating Bits.py
@@ -1,4 +1,12 @@
+#use highest bit trick
+def hasAlternatingBits(self, n):
+    h = n
+    for i in range(5):
+        h |= h>>1  
 
+    return h == n^(n>>1)
+
+#slower, go bit by bit
 def hasAlternatingBits(self, n):
 
     while n:
