b9f717069d09e63da49cbd4399a5c3e7070548a5
Mc Fair <mcfair18@gmail.com>
2018-10-02 06:44:12 -0700

    Update 260. Single Number III.py

--- Bit/260. Single Number III.py
+++ Bit/260. Single Number III.py
@@ -4,16 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        a, b = 0, 0 # store the result to the problem
+        a = b = 0 # store the result to the problem
         
         #solution for Leetcode136 Single Number
         #here it returns the XOR result of two single numbers 
         mask = reduce(operator.xor, nums)
         
-        #get the lowest set bit, and we know that a, b are different at this bit
+        #get the lowest set bit (bit with value 1), and it is where a and b differ, i.e. mask = a^b
         mask = self.lowbit(mask) 
         
-        #Now, nums can be partitioned into two groups according to their bits at location i.
+        #Now, nums can be partitioned into two groups by using their bits at location i.
         #the first group consists of all numbers whose bits at i is 0.
         #the second group consists of all numbers whose bits at i is 1.
         #even occurences in each group are cancelled (like in Leetcode136), only left out single number
