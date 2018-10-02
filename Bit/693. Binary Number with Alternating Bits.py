fd8c906abbb90106bbe9bf42931951f064bd9463
Mc Fair <mcfair18@gmail.com>
2018-10-02 11:40:05 -0700

    Update 693. Binary Number with Alternating Bits.py

--- Bit/693. Binary Number with Alternating Bits.py
+++ Bit/693. Binary Number with Alternating Bits.py
@@ -1,3 +1,16 @@
+"""
+def highestBit2(x):
+    # Suppose n is 273 (binary  
+    # is 100010001). It does following 
+    # 100010001 | 010001000 = 110011001 
+    n |= n>>1
+    # 110011001 | 001100110 = 111111111 
+    n |= n>>2   
+    n |= n>>4  
+    n |= n>>8
+    n |= n>>16
+"""
+
 #if it's alternating 1s and 0s, n^(n>>1) should give 000011...1
 #use highest bit trick to generate that number, and compare to n^(n-1)
 def hasAlternatingBits(self, n):
