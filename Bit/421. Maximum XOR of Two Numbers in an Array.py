632798c2d4385d4994de5580a598c7335119e6d9
Mc Fair <mcfair18@gmail.com>
2018-10-02 16:23:55 -0700

    Update 421. Maximum XOR of Two Numbers in an Array.py

--- Bit/421. Maximum XOR of Two Numbers in an Array.py
+++ Bit/421. Maximum XOR of Two Numbers in an Array.py
@@ -1,4 +1,48 @@
-#don't understand yet
+"""
+Subtleties behind this brilliant algorithm:
+
+Bit masking
+Greedy search based on prefix
+Variation of 2sum: see below for the analogy
+
+The classic 2sum problem actually achieved the acceleration from O(N^2) to O(N) by utilizing hashset's O(1) lookup time.
+
+In 2sum, we want to find two numbers so that a + b = sum, naive way is to generate all pairs, and test each pair. 
+That takes time O(N^2). Instead, we only look at one number at each iteration, and try to find sum - a in the HashMap.
+
+In this problem (the second part where we try to update ans): we want to find two prefixs out of the hashset, 
+such that prefix1 ^ prefix2 = tmp, with tmp being the greedily updated ans. Similiary to 2sum, 
+we only look at one prefix at each iteration, and try to find  cand ^ prefix in the hashset. 
+
+Note that A ^ B = tmp is equivalent to A = tmp ^ B,  A ^ tmp = B.
+"""
+class Solution(object):
+    def findMaximumXOR(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ans = mask = 0
+        
+        for i in range(32, -1, -1):
+            mask |= (1 << i)
+            hashset_ = set()
+            for num in nums:
+                hashset_.add(num & mask)
+            cand = ans | (1 << i)  
+            # ans is the maximum we can get if we consider only the most significant i - 1 bits
+            # cand the potential max value we can get when considering the most significant i bits. 
+            for prefix in set_:
+                if cand ^ prefix in hashset_:
+                    ans = cand
+                    break
+        return ans
+    
+    
+
+
+#understand the trie idea
+#but don't understand the max matching for loop
 class Solution(object):
     def findMaximumXOR(self, nums):
         """
