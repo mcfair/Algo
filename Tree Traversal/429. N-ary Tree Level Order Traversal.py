68e5f89b1e71f26ad751169e3b8c10862aa70ac3
Mc Fair <mcfair18@gmail.com>
2018-10-03 15:57:32 -0700

    Update 429. N-ary Tree Level Order Traversal.py

--- Tree Traversal/429. N-ary Tree Level Order Traversal.py
+++ Tree Traversal/429. N-ary Tree Level Order Traversal.py
@@ -1,1 +1,15 @@
 
+class Solution(object):
+    def levelOrder(self, root):
+ 
+        if not root: return []
+        stack = [root]
+        res = []
+        while stack:
+            res.append([node.val for node in stack])
+            next_row = []
+            for node in stack:
+                next_row.extend(node.children)
+            stack = next_row
+        return res
+        
