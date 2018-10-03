afc746588927b6442959e38ce081638541051e1e
Mc Fair <mcfair18@gmail.com>
2018-10-03 14:57:33 -0700

    Update 99. Recover Binary Search Tree.py

--- BST/99. Recover Binary Search Tree.py
+++ BST/99. Recover Binary Search Tree.py
@@ -7,7 +7,7 @@ def inorderIter(root):
         yield root        
         for node in inorderIter(root.right): 
             yield node
-            
+    
 class Solution(object):
     def recoverTree(self, root):
         prev, first, second = None, None, None
@@ -26,3 +26,40 @@ class Solution(object):
  
 #O(n) time, O(1) space, Morris Traversal
 #https://www.jianshu.com/p/d2059062efac
+def morris_in_order(root):
+    curr = root
+    while curr:
+        if not curr.left:
+            yield curr 
+            curr = curr.right
+        else:
+            # find current node's predecessor = rightmost node of the left subtree
+            pred = curr.left 
+            while pred.right and pred.right!= curr:
+                pred = pred.right
+
+            #morris threading: use predecessor's right pointer to connect with current node
+            if not pred.right:
+                pred.right = curr
+                curr = curr.left
+            #if there is a connection, visit the current node then delete the threading
+            else:
+                yield curr 
+                pred.right = None
+                curr = curr.right
+  
+#same driver function as line 11           
+class Solution(object):
+    def recoverTree(self, root):       
+        prev, first, second = None, None, None
+        for curr in morris_in_order(root):
+            if prev and prev.val > curr.val:
+                if not first:
+                    first = prev
+                    second = curr
+                else:
+                    second = curr
+            prev = curr
+         
+        first.val, second.val = second.val, first.val
+         
