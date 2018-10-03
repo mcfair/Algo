3eb59514bd9cd7db5381447216dcf3872eac8d78
Mc Fair <mcfair18@gmail.com>
2018-10-03 15:38:18 -0700

    Update 590. N-ary Tree Postorder Traversal.py

--- Tree Traversal/590. N-ary Tree Postorder Traversal.py
+++ Tree Traversal/590. N-ary Tree Postorder Traversal.py
@@ -1,1 +1,11 @@
 
+class RecursiveSolution(object):
+    def postorder(self, root):
+        if not root: return []
+  
+        res = []
+        for kid in root.children:
+            res += self.postorder(kid)
+        res.append(root.val)
+        
+        return res
