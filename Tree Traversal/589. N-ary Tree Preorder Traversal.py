fb9247d16e9c4304e8ed9e6b7b44c6df8ba36a24
Mc Fair <mcfair18@gmail.com>
2018-10-03 15:41:54 -0700

    Update 589. N-ary Tree Preorder Traversal.py

--- Tree Traversal/589. N-ary Tree Preorder Traversal.py
+++ Tree Traversal/589. N-ary Tree Preorder Traversal.py
@@ -1,22 +1,22 @@
 #Recursive solution is trivial
 class RecursiveSolution(object):
     def preorder(self, root):
-        if not root: 
-            return []
-        t=[root.val]
+        if not root: return []
+        
+        res=[root.val]
         for i in root.children:
-            t+=self.preorder(i)
-        return t
+            res += self.preorder(i)
+        return res
         
  #Iterative Solution
  class IterativeSolution(object):
     def preorder(self, root):
+        if not root: return []
         stack = [root]
         res = []
         while stack:
             p=stack.pop()
-            if p:
-                res.append(p.val)
-                for i in reversed(range(len(p.children))): 
-                    stack.append(p.children[i])
+            res.append(p.val)
+            for i in reversed(range(len(p.children))): 
+                stack.append(p.children[i])
         return res
