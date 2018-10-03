97dd0671e5612730355e22c31e9f4c14b2c192a5
Mc Fair <mcfair18@gmail.com>
2018-10-03 14:38:37 -0700

    Create 000. Morris Traversal.py

---
+++ Tree Traversal/000. Morris Traversal.py
@@ -0,0 +1,48 @@
+def morris_in_order(root):
+    curr = root
+    while curr:
+        if not curr.left:
+            print curr.val
+            curr = curr.right
+        else:
+            # find current node's predecessor = rightmost node of the left subtree
+            pred = curr.left 
+            while pred.right and pred.right!= curr:
+                pred = pred.right
+             
+            #morris threading: use predecessor's right pointer to connect with current node
+            if not pred.right:
+                pred.right = curr
+                curr = curr.left
+            #if there is a connection, visit the current node then delete the threading
+            else:
+                print curr.val
+                pred.right = None
+                curr = curr.right
+                
+                
+                
+ def morris_pre_order(root):
+    curr = root
+    while curr:
+        if not curr.left:
+            print curr.val
+            curr = curr.right
+        else:
+            # find current node's predecessor = rightmost node of the left subtree
+            pred = curr.left 
+            while pred.right and pred.right!= curr:
+                pred = pred.right
+             
+            #morris threading: use predecessor's right pointer to connect with current node
+            if not pred.right:
+                print curr.val
+                pred.right = curr
+                curr = curr.left
+            #if there is a connection, visit the current node then delete the threading
+            else:
+                pred.right = None
+                curr = curr.right
+                               
+                
+  
