3d6d301670f3b017962ae2cfbd765da6164d19e6
Mc Fair <mcfair18@gmail.com>
2018-10-03 16:04:40 -0700

    Update 107. BT Level Order Traversal II.py

--- Tree Traversal/107. BT Level Order Traversal II.py
+++ Tree Traversal/107. BT Level Order Traversal II.py
@@ -1,1 +1,10 @@
-
+class Solution(object):
+    def levelOrderBottom(self, root):
+        
+        if not root: return []
+        bfs, res = [root], []
+        while bfs:
+            res.append([node.val for node in bfs])
+            bfs = [kid for node in bfs for kid in (node.left,node.right) if kid]
+        return res[::-1]
+        
