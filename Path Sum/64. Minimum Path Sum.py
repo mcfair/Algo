4e183917d769a5a138fcc9cb8f7055a39f9f0329
Mc Fair <mcfair18@gmail.com>
2018-10-03 10:24:06 -0700

    Update 64. Minimum Path Sum.py

--- Path Sum/64. Minimum Path Sum.py
+++ Path Sum/64. Minimum Path Sum.py
@@ -1,4 +1,9 @@
- 
+#Asked in Amazon on-site 09/21
+"""
+This is array problem, not a tree problem.
+Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, 
+which minimizes the sum of all numbers along its path.
+"""
 def minPathSum(self, grid):
     """
     :type grid: List[List[int]]
@@ -19,3 +24,33 @@ def minPathSum(self, grid):
             dp[i][j] += min(dp[i-1][j], dp[i][j-1]) 
 
     return dp[m-1][n-1]
+
+ """
+ Amazon's Follow-up to backtrack the actual path.
+ """
+ class Solution(object):
+    def minPathSum(self, grid):
+
+        m, n = len(grid), len(grid[0])
+        dp = collections.defaultdict(lambda:float('inf'))
+        parent = {}
+       
+        for i in range(m):
+            for j in range(n):   
+                dp[i,j]=grid[i][j]
+        
+        for i in range(m):
+            for j in range(n):    
+                if (i,j)!=(0,0): 
+                    if dp[i-1,j] < dp[i,j-1]:
+                        parent[i,j] = (i-1,j)
+                        dp[i,j] +=  dp[i-1,j]
+                    else:
+                        parent[i,j] = (i,j-1)
+                        dp[i,j] +=  dp[i,j-1]
+       
+        path = [(m-1,n-1)]            
+        while path[0]!=(0,0):
+            path =[parent[path[0]]] + path
+  
+        return dp[m-1,n-1]
