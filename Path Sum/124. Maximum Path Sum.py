928838c692686c3c59a839997cd897ba112dcd30
Mc Fair <mcfair18@gmail.com>
2018-10-03 09:52:33 -0700

    Update 124. Maximum Path Sum.py

--- Path Sum/124. Maximum Path Sum.py
+++ Path Sum/124. Maximum Path Sum.py
@@ -5,7 +5,7 @@ we use path_sum to update self.maxsum
 
 One important thing is that: it's NOT useful to return the path sum in the DFS function,
 instead, we need to return current_node_val + max(0, left_child_sum, right_child_sum)
-that's because in next round recursion, we need sum of a sub branch (from a node to its child, to its grandchild ...)
+that's because in next round recursion, we need the sum of a sub branch (from a node to its child, to its grandchild ...)
 """
 class Solution(object):
     def maxPathSum(self, root):
