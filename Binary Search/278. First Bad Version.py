e557c6c4196e5f444c9ab5d6ddd97fd52c47f6ea
Mc Fair <mcfair18@gmail.com>
2018-10-04 13:35:10 -0700

    Update 278. First Bad Version.py

--- Binary Search/278. First Bad Version.py
+++ Binary Search/278. First Bad Version.py
@@ -1,6 +1,6 @@
 
 def firstBadVersion(self, n):
-    l, r = 1, n+1                  #search in [l, r) range
+    l, r = 1, n+1                  #search in [l, r+1) range, both r=n and r=n+1 works for this problem...
     while l<r:
         mid = (l+r)/2
         if isBadVersion(mid):
