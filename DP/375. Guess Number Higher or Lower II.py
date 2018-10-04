9d17f558a18f00b44879a52d37da420f847076ec
Mc Fair <mcfair18@gmail.com>
2018-10-04 12:08:19 -0700

    Update 375. Guess Number Higher or Lower II.py

--- DP/375. Guess Number Higher or Lower II.py
+++ DP/375. Guess Number Higher or Lower II.py
@@ -1,1 +1,19 @@
+#Top down, DFS + Memo
+def getMoneyAmount(self, n):
+    memo = collections.defaultdict(int)
+    def dfs(lo, hi):
+        if lo>=hi: 
+            return 0
+        if (lo,hi) in memo:
+            return memo[lo,hi]
 
+        res = float('inf')
+        for i in range(lo,hi):
+            res = min(res,i + max(dfs(lo,i-1), dfs(i+1, hi)))
+
+        memo[lo,hi] = res
+        return res
+
+    return dfs(1,n)
+  
+ #Bottom up, DP
