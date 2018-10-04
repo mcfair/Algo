8a222f07d2293ec2d38b637a8fff96a512c0b9dd
Mc Fair <mcfair18@gmail.com>
2018-10-03 21:53:28 -0700

    Update 002. Recursive Traversal.py

--- Tree Traversal/002. Recursive Traversal.py
+++ Tree Traversal/002. Recursive Traversal.py
@@ -1,29 +1,23 @@
 def preorderTraversal(root):
     if not root: return []
     res = []
-    
     res.append(root.val)
     res.extend(self.preorderTraversal(root.left))
     res.extend(self.preorderTraversal(root.right))
-    
     return res
   
 def inorderTraversal(root):
     if not root: return []
     res = []
-
     res.extend(self.inorderTraversal(root.left))
     res.append(root.val)
     res.extend(self.inorderTraversal(root.right))
-
     return res 
 
 def postorderTraversal(root):
     if not root: return []
     res = []
-    
     res.extend(self.postorderTraversal(root.left))
     res.extend(self.postorderTraversal(root.right))
     res.append(root.val)
-    
     return res
