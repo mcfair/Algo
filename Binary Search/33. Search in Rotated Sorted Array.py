ecf9feddd8465c4c2a636d807bc7100112d10ee8
Mc Fair <mcfair18@gmail.com>
2018-10-04 07:18:44 -0700

    Create 33. Search in Rotated Sorted Array.py

---
+++ Binary Search/33. Search in Rotated Sorted Array.py
@@ -0,0 +1,21 @@
+def search(self, nums, target):
+    l, r = 0, len(nums)
+    pivot = nums[0]
+    
+    while l < r:
+        mid = (l+r)//2
+        if (target< pivot) == (nums[mid]<pivot): #target and nums[mid] in the same zone
+            comparator = nums[mid]
+        else:
+            if target < pivot: 
+                comparator = float('-inf')
+            else:
+                comparator = float('inf')
+         #regular binary search
+         if target == comparator: return mid
+         if target < comparator:
+            l = mid+1
+         else:
+            r = mid
+     return l
+         
