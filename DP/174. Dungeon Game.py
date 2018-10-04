11fb739a49b83a45dfdc05900119fbdc1a9ed6a4
Mc Fair <mcfair18@gmail.com>
2018-10-04 14:42:11 -0700

    Update 174. Dungeon Game.py

--- DP/174. Dungeon Game.py
+++ DP/174. Dungeon Game.py
@@ -4,7 +4,7 @@ DFS + Memo
 Single DP from bottom-right corner to top-left corner
 Double DP, maintain two matrix: minInitHP, currHP 
 """
-class DFSSolution(object):
+class DFS_Solution(object):
     def calculateMinimumHP(self, dungeon):
 
         def dfs(i,j):
@@ -23,3 +23,19 @@ class DFSSolution(object):
         memo = {} #min_init_HP required to reach certain cell
         return dfs(0,0)
         
+#single DP with 2D array
+#dp[i][j] = minimum health level required to reach the princess when entering (i, j)
+#https://leetcode.com/problems/dungeon-game/discuss/52826/a-very-clean-and-intuitive-solution-with-explanation
+class DP_Solution(object):
+    def calculateMinimumHP(self, dungeon):
+        m, n = len(dungeon), len(dungeon[0])        
+        minInitHP = collections.defaultdict(lambda:float('inf'))
+        minInitHP[m,n-1] = 0
+        minInitHP[m-1,n] = 0
+        
+        for i in range(m)[::-1]:
+            for j in range(n)[::-1]:
+                minInitHP[i,j] = min(minInitHP[i+1,j], minInitHP[i,j+1]) - dungeon[i][j]
+                minInitHP[i,j] = max(0, minInitHP[i,j]) #HP can't be negative
+                    
+        return minInitHP[0,0] + 1
