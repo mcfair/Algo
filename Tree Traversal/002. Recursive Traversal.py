ac0beca97bd3b3c6a2e55b5e911c61886d9893bc
Mc Fair <mcfair18@gmail.com>
2018-10-03 22:01:53 -0700

    Update 002. Recursive Traversal.py

--- Tree Traversal/002. Recursive Traversal.py
+++ Tree Traversal/002. Recursive Traversal.py
@@ -1,3 +1,6 @@
+"""
+direct recursion
+"""
 def preorderTraversal(root):
     if not root: return []
     res = []
@@ -21,3 +24,29 @@ def postorderTraversal(root):
     res.extend(self.postorderTraversal(root.right))
     res.append(root.val)
     return res
+
+"""
+recursive generator
+"""
+def preorderGen(root):
+    if root:
+        yield root
+        for x in preorderGen(root.left): yield x
+        for x in preorderGen(root.right): yield x
+        
+        
+def inorderGen(root):
+    if root:
+        for x in inorderGen(root.left): yield x
+        yield root
+        for x in inorderGen(root.right): yield x
+        
+def postorderGen(root):
+    if root:
+        for x in postorderGen(root.left): yield x
+        for x in postorderGen(root.right): yield x
+        yield root
+        
+#driver function example
+def postorderTraversal(root):
+    return [x.val for x in postorderGen(root)] 
