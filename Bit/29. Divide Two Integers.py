0a1d967c08f89fdbcdd4aafa6c11d221aa3b93ca
Mc Fair <mcfair18@gmail.com>
2018-10-04 06:15:38 -0700

    Create 29. Divide Two Integers.py

---
+++ Bit/29. Divide Two Integers.py
@@ -0,0 +1,14 @@
+"""
+Divide a by b, without using multiplication, division and mod operators.
+Integer division should truncate toward zero.
+"""
+def divide(self, a, b):
+    sig = (a < 0) == (b < 0)
+    a, b, res = abs(a), abs(b), 0
+    while a >= b:
+        x = 0
+        while a >= b << (x + 1): 
+          x += 1
+        res += 1 << x
+        a -= b << x
+    return min(res if sig else -res, 2147483647)
