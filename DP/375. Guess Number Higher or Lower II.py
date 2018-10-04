9c0586283cdd8bf12008ec8f03bab3e2b5b7c4fe
Mc Fair <mcfair18@gmail.com>
2018-10-04 12:36:21 -0700

    Update 375. Guess Number Higher or Lower II.py

--- DP/375. Guess Number Higher or Lower II.py
+++ DP/375. Guess Number Higher or Lower II.py
@@ -8,9 +8,11 @@ def getMoneyAmount(self, n):
             return memo[lo,hi]
 
         res = float('inf')
+        #take "i" that cost the least
+        #for each choice of i, the cost = max(dfs(lo,i-1)+i, dfs(i+1, hi)+i)
         for i in range(lo,hi):
-            res = min(res,i + max(dfs(lo,i-1), dfs(i+1, hi)))
-
+            res = min(res, i + max(dfs(lo,i-1), dfs(i+1, hi)))
+            
         memo[lo,hi] = res
         return res
 
