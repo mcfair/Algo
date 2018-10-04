dba4c807e529d5aa21539b777264a8bac525534a
Mc Fair <mcfair18@gmail.com>
2018-10-04 14:56:56 -0700

    Update 174. Dungeon Game.py

--- DP/174. Dungeon Game.py
+++ DP/174. Dungeon Game.py
@@ -22,10 +22,11 @@ class DFS_Solution(object):
         m, n = len(dungeon), len(dungeon[0])        
         memo = {} #min_init_HP required to reach certain cell
         return dfs(0,0)
-        
-#single DP with 2D array
-#dp[i][j] = minimum health level required to reach the princess when entering (i, j)
-#https://leetcode.com/problems/dungeon-game/discuss/52826/a-very-clean-and-intuitive-solution-with-explanation
+"""        
+single DP with 2D array
+dp[i][j] = minimum health level required to reach the princess when entering (i, j)
+https://leetcode.com/problems/dungeon-game/discuss/52826/a-very-clean-and-intuitive-solution-with-explanation
+"""
 class DP_Solution(object):
     def calculateMinimumHP(self, dungeon):
         m, n = len(dungeon), len(dungeon[0])        
@@ -39,3 +40,28 @@ class DP_Solution(object):
                 minInitHP[i,j] = max(0, minInitHP[i,j]) #HP can't be negative
                     
         return minInitHP[0,0] + 1
+
+"""
+Double DP
+Two arrays are used: one for saving minimum cost, one for saving minimum knight HP
+https://leetcode.com/problems/dungeon-game/discuss/52784/Who-can-explain-why-%22from-the-bottom-right-corner-to-left-top.%22
+"""
+class Solution(object):
+    def calculateMinimumHP(self, dungeon):
+        m, n = len(dungeon), len(dungeon[0])        
+        minInitHP = collections.defaultdict(lambda:float('inf'))
+        minCost = collections.defaultdict(int)
+        
+        for i in range(m):
+            for j in range(n):
+                if (i,j)==(0,0):
+                    minCost[0,0] = dungeon[0][0]
+                    minInitHP[0,0] = max(1, 1- minCost[0,0])
+                else:
+                    minCost[i,j] = dungeon[i][j] + max(minCost[i-1,j], minCost[i,j-1])
+                    minInitHP[i,j] = min(
+                        max(minInitHP[i-1,j], 1 - minCost[i-1,j] - dungeon[i][j]),
+                        max(minInitHP[i,j-1], 1 - minCost[i,j-1] - dungeon[i][j]),
+                        )
+                    
+        return minInitHP[m-1,n-1]
