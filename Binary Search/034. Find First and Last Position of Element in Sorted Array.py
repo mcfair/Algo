71ea89b0701e74a760a6fc484af18cc4f95f6182
Mc Fair <mcfair18@gmail.com>
2018-10-04 13:28:28 -0700

    Update 034. Find First and Last Position of Element in Sorted Array.py

--- Binary Search/034. Find First and Last Position of Element in Sorted Array.py
+++ Binary Search/034. Find First and Last Position of Element in Sorted Array.py
@@ -1,4 +1,4 @@
-#naive single binary search: average O(logn), worst case O(n)
+#Method(1) naive single binary search: average O(logn), worst case O(n)
 def searchRange(self, nums, target):
       l, r = 0, len(nums)
       while l<r:
@@ -16,32 +16,57 @@ def searchRange(self, nums, target):
               r = mid
       return [-1,-1]
 
-#we can use two binary search to find left boundary and right boundary respectively
-class Solution:
-    def searchRange(self, nums, target):
-        if not nums:
-            return [-1, -1]
+#Method(2) we can use two binary search to find left boundary and right boundary respectively
+def bisect_left(nums, target):
+      l, r = 0, len(nums) -1
+      while l < r:
+          m = (l + r) / 2
+          if nums[m] < target:
+              l = m + 1
+          else:
+              r = m
+      return l if nums[l] == target else -1
 
-        def bisect_left(nums, target):
-            l, r = 0, len(nums) -1
-            while l < r:
-                m = (l + r) / 2
-                if nums[m] < target:
-                    l = m + 1
-                else:
-                    r = m
-            return l if nums[l] == target else -1
+def bisect_right(nums, target): 
+      l, r = 0, len(nums) -1
+      while l < r:
+          m = (l + r) / 2 + 1  # Make mid biased to the right
+          if nums[m] > target:
+              r = m - 1
+          else:
+              l = m
+      return l if nums[l] == target else -1
 
-        def bisect_right(nums, target):
-            l, r = 0, len(nums) -1
-            while l < r:
-                m = (l + r) / 2 + 1  # Make mid biased to the right
-                if nums[m] > target:
-                    r = m - 1
-                else:
-                    l = m
-            return l if nums[l] == target else -1
-
+def searchRange(self, nums, target):
+        if not nums: return [-1, -1]
         return [bisect_left(nums, target), bisect_right(nums, target)]
-                
-                
+
+#Method(3) actually we can use bisect_left only, don't have to be confused with the revised bisect_right
+def bisect_left(nums, target):
+      l, r = 0, len(nums)
+      while l < r:
+          m = (l + r) / 2
+          if nums[m] < target:
+              l = m + 1
+          else:
+              r = m
+      return l  
+
+
+def searchRange(self, nums, target):
+        if not nums: return [-1, -1]
+        lo = bisect_left(nums,target)
+        return [lo, bisect_left(nums,target+1)-1] if target in nums[lo:lo+1] else [-1, -1]
+
+#Method(4) use python libary
+class Solution(object):
+    def searchRange(self, nums, target):
+        if not nums: 
+            return [-1,-1]
+        
+        lo = bisect.bisect_left(nums, target)
+        
+        if target not in nums[lo:lo+1]: #use list slice to avoid out-of-range error
+            return [-1,-1]
+        
+        return [lo, bisect.bisect_right(nums, target, lo=lo)-1]  
