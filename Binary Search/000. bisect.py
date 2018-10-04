019afdea764ea920060c697e3bc65e1729a76fab
Mc Fair <mcfair18@gmail.com>
2018-10-04 13:13:10 -0700

    Update 000. bisect.py

--- Binary Search/000. bisect.py
+++ Binary Search/000. bisect.py
@@ -3,8 +3,7 @@ def bisect_left(a, target, lo=0, hi=None):
     The return value i is such that all e in a[:i] have e < x, and all e in
     a[i:] have e >= x.  So if x already appears in the list, a.insert(x) will
     insert just before the leftmost x already there.
-    Optional args lo (default 0) and hi (default len(a)) bound the
-    slice of a to be searched.
+    Optional args lo (default 0) and hi (default len(a))
     """
 
     if lo < 0:
@@ -24,8 +23,7 @@ def bisect_right(a, target, lo=0, hi=None):
     The return value i is such that all e in a[:i] have e <= x, and all e in
     a[i:] have e > x.  So if x already appears in the list, a.insert(x) will
     insert just after the rightmost x already there.
-    Optional args lo (default 0) and hi (default len(a)) bound the
-    slice of a to be searched.
+    Optional args lo (default 0) and hi (default len(a))  
     """
 
     if lo < 0:
@@ -39,3 +37,25 @@ def bisect_right(a, target, lo=0, hi=None):
         else: 
             lo = mid+1
     return lo
+
+## Another implementation using hi=len(a)-1, instead of hi=len(a)
+
+def bisect_left(nums, target):
+    l, r = 0, len(nums) -1
+    while l < r:
+        m = (l + r) / 2
+        if nums[m] < target:
+            l = m + 1
+        else:
+            r = m
+    return l if nums[l] == target else -1
+
+def bisect_right(nums, target):
+    l, r = 0, len(nums) -1
+    while l < r:
+        m = (l + r) / 2 + 1  # Make mid biased to the right
+        if nums[m] > target:
+            r = m - 1
+        else:
+            l = m
+    return l if nums[l] == target else -1
