89040751183264635c62eaed82647e38d9a403ec
Mc Fair <mcfair18@gmail.com>
2018-10-05 14:35:25 -0700

    Update 719. Find K-th Smallest Pair Distance.py

--- Binary Search/719. Find K-th Smallest Pair Distance.py
+++ Binary Search/719. Find K-th Smallest Pair Distance.py
@@ -1,5 +1,27 @@
-#Binary Search + Counting   (TLE....)
+#Binary Search + Counting (another binary search)
 
+class Solution(object):
+    def smallestDistancePair(self, nums, k):
+        nums.sort() 
+        n = len(nums)
+        #search on the diffrence space, not on nums
+        lo, hi = 0, nums[-1] - nums[0]
+        
+        while lo<hi:
+            mid = (lo+hi)//2
+            
+            count = 0
+            for i in range(n):
+                j = bisect.bisect_right(nums, nums[i]+mid)
+                count += j-i-1
+            
+            if count < k:
+                lo = mid+1
+            else:
+                hi = mid
+        return lo
+    
+# TLE version, while loop is O(n), while bisect_right is O(logn)
 def smallestDistancePair(self, nums, k):
 
     nums.sort() 
