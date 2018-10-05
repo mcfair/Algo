00c112e90ce231ee02f466ca150aae254967aaba
Mc Fair <mcfair18@gmail.com>
2018-10-05 12:46:05 -0700

    Update 378. Kth Smallest Element in a Sorted Matrix.py

--- Heap/378. Kth Smallest Element in a Sorted Matrix.py
+++ Heap/378. Kth Smallest Element in a Sorted Matrix.py
@@ -9,7 +9,7 @@ Same method is used to solve LC23. Merge k Sorted Lists.
 def kthSmallest(self, matrix, k):
         heap = [(row[0], i, 0) for i, row in enumerate(matrix)]
         heapq.heapify(heap)
-        ret = 0
+      
         for _ in range(k):
             ret, i, j = heapq.heappop(heap)
             if j+1 < len(matrix[0]):
