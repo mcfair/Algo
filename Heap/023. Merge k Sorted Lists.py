bef5deaabfdfcfef5c34793433376b9986250c27
Mc Fair <mcfair18@gmail.com>
2018-10-05 12:40:00 -0700

    Update and rename 23. Merge k Sorted Lists.py to 023. Merge k Sorted Lists.py

--- Heap/023. Merge k Sorted Lists.py
+++ Heap/023. Merge k Sorted Lists.py
@@ -1,9 +1,6 @@
 class Solution(object):
     def mergeKLists(self, lists):
-        """
-        :type lists: List[ListNode]
-        :rtype: ListNode
-        """
+
         if not lists: return []
         if len(lists)==1: return lists[0]
         
@@ -20,3 +17,8 @@ class Solution(object):
                 heapq.heappush(q, (node.next.val, node.next))
             
         return ret.next        
+ 
+class ListNode(object):
+     def __init__(self, x):
+         self.val = x
+         self.next = None
