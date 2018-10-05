384e25c387d4247f995cdf05b3b5447ef2eb159f
Mc Fair <mcfair18@gmail.com>
2018-10-05 14:41:58 -0700

    Update 719. Find K-th Smallest Pair Distance.py

--- Heap/719. Find K-th Smallest Pair Distance.py
+++ Heap/719. Find K-th Smallest Pair Distance.py
@@ -11,3 +11,30 @@ def smallestDistancePair(self, nums, k):
         v = heapq.heappop(q)
     return v
 
+"""
+Binary Search inside binary search
+"""
+def smallestDistancePair(self, nums, k):
+
+    nums.sort() 
+    n = len(nums)
+    #search on the diffrence space, not on nums
+    lo, hi = 0, nums[-1] - nums[0]
+
+    while lo<hi:
+        mid = (lo+hi)//2
+
+        count = 0
+        for i in range(n):
+            j = bisect.bisect_right(nums, nums[i]+mid)
+            count += j-i-1
+
+        if count < k:
+            lo = mid+1
+        else:
+            hi = mid
+
+    return lo
+
+                
+            
