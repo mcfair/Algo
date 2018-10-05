322068501dc6e4fa9e1cb60401e5986159ae4130
Mc Fair <mcfair18@gmail.com>
2018-10-05 09:11:01 -0700

    Update 628. Max Product of Three Numbers.py

--- Heap/628. Max Product of Three Numbers.py
+++ Heap/628. Max Product of Three Numbers.py
@@ -10,6 +10,7 @@ class Solution(object):
         p2 = nums[-3]*nums[-2]*nums[-1]
         return max(p1, p2)
         
+#heap
 class Solution(object):
     def maximumProduct(self, nums):
         top3, min2 = [], []
@@ -25,3 +26,25 @@ class Solution(object):
         p2 = min2[0]*min2[1]*max(top3)
         return max(p1,p2)
             
+#manually find 5 variables
+class Solution(object):
+    def maximumProduct(self, nums):
+        min1, min2 = float('inf'), float('inf')
+        max1, max2, max3 = float('-inf'), float('-inf'), float('-inf')
+        for num in nums:
+            if min2 < num < max3:
+                continue
+            if num < min1:
+                min1, min2 = num, min1
+            elif num < min2:
+                min2 = num
+            if num > max1:
+                max1, max2, max3 = num, max1, max2
+            elif num > max2:
+                max2, max3 = num, max2
+            elif num > max3:
+                max3 = num
+                
+        p1 = min1*min2*max1
+        p2 = max1*max2*max3
+        return max(p1,p2)
