fd94fea48998eb4f0185c660f84b2d0eff538f7d
Mc Fair <mcfair18@gmail.com>
2018-10-05 16:25:24 -0700

    Update 668. K-th Smallest Number in Multiplication Table.py

--- Binary Search/668. K-th Smallest Number in Multiplication Table.py
+++ Binary Search/668. K-th Smallest Number in Multiplication Table.py
@@ -9,18 +9,18 @@ For this particular problem, we know each row is [1, 2, 3, .., n]*i
 If there are x number of elements less than mid, it satisfies x < mid/i, so x <= mid/i
 So the count of each row is simply min(mid/i, n)
 """
-class Solution(object):
-    def findKthNumber(self, m, n, k):
  
-        l, r = 1, m*n
-        while l<r:
-            mid = (l+r)/2
-            
-            #see explanation
-            count = sum(min(mid/i, n) for i in range(1,m+1))
-            
-            if count < k:
-                l = mid+1
-            else:
-                r = mid
-        return l
+def findKthNumber(self, m, n, k):
+
+    l, r = 1, m*n
+    while l<r:
+        mid = (l+r)/2
+
+        #see explanation
+        count = sum(min(mid/i, n) for i in range(1,m+1))
+
+        if count < k:
+            l = mid+1
+        else:
+            r = mid
+    return l
