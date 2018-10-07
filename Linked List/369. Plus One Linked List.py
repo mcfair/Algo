33dc03d8be6f9bc9bb6fa597f91291aa4fd6cbe8
Mc Fair <mcfair18@gmail.com>
2018-10-07 13:13:00 -0700

    Update 369. Plus One Linked List.py

--- Linked List/369. Plus One Linked List.py
+++ Linked List/369. Plus One Linked List.py
@@ -1,3 +1,20 @@
+#Stephan way...
+def plusOne(self, head):
+    def reverse(head):
+        rev = None
+        while head:
+            head.next, head, rev = rev, head.next, head
+        return rev
+    head = node = reverse(head)
+    while node.val == 9:
+        node.val = 0
+        if not node.next:
+            node.next = ListNode(0)
+        node = node.next
+    node.val += 1
+    return reverse(head)
+
+
 class Solution(object):
     def plusOne(self, head):
         """
