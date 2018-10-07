625c396a45855e3a6abc43e91093301b69e384f1
Mc Fair <mcfair18@gmail.com>
2018-10-07 08:33:16 -0700

    Update 2. Add Two Numbers.py

--- Linked List/2. Add Two Numbers.py
+++ Linked List/2. Add Two Numbers.py
@@ -1,8 +1,36 @@
+#Stephan add any number of lists
+
 def addTwoNumbers(self, l1, l2):
+    addends = [l1, l2]
+    dummy = res = ListNode(0)
+    carry = 0
+    while addends or carry:
+        carry += sum(a.val for a in addends)
+        addends = [a.next for a in addends if a.next]
+        res.next =  ListNode(carry % 10)
+        res = res.next
+        carry /= 10
+    return dummy.next
 
-    if not l1 or not l2:
-        return l1 or l2
+#concise add two lists
+def addTwoNumbers(self, l1, l2):
+    dummy = cur = ListNode(0)
+    carry = 0
+    while l1 or l2 or carry:
+        if l1:
+            carry += l1.val
+            l1 = l1.next
+        if l2:
+            carry += l2.val
+            l2 = l2.next
+        cur.next = ListNode(carry%10)
+        cur = cur.next
+        carry //= 10
+    return dummy.next
 
+#my verbose implementation
+def addTwoNumbers(self, l1, l2):
+ 
     head = res = ListNode(None)
     carry = 0
     while l1 or l2:
