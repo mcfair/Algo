57fd49e480690ce941290711079d5706290bc5ce
Mc Fair <mcfair18@gmail.com>
2018-10-10 08:12:00 -0700

    Update and rename Sort/057. Insert Interval.py to Interval/057. Insert Interval.py

--- Interval/057. Insert Interval.py
+++ Interval/057. Insert Interval.py
@@ -1,3 +1,10 @@
+"""
+Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+You may assume that the intervals were initially sorted according to their start times.
+
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+"""
 
 class Solution(object):
     def insert(self, intervals, newInterval):
@@ -13,6 +20,7 @@ class Solution(object):
         left = [i for i in intervals if i.end < s]
         right = [i for i in intervals if i.start > e]
         
+        #this "if" statement handles overlapping intervals
         if len(left) + len(right) != len(intervals):
             s = min(s, intervals[len(left)].start)
             e = max(e, intervals[~len(right)].end)
