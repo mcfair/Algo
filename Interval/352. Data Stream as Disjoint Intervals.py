fc9844618ec235f560fdafc1df305adc25aeaf0c
Mc Fair <mcfair18@gmail.com>
2018-10-11 12:12:53 -0700

    Update 352. Data Stream as Disjoint Intervals.py

--- Interval/352. Data Stream as Disjoint Intervals.py
+++ Interval/352. Data Stream as Disjoint Intervals.py
@@ -1,19 +1,15 @@
 """
 thought process:
 what's the best data structure to maintain this interval array, such that it has low cost to add numbers.
-List will have addNum O(n) and getInterval O(1) --> good design for less write/add and heavy read/query
-BST will have addNum O(lgn) and getInterval O(n) --> good design for heavy write and light query
+List will have addNum O(N) and getInterval O(1) --> good design for less write/add and heavy read/query
+BST will have addNum O(lgN) and getInterval O(N) --> good design for heavy write and light query
+Heap implementation will give O(lgN) addNum and O(NlgN) query.
 """
 #List solution O(n) Add, O(1) Query
 class SummaryRanges(object):
-
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.memo = []
         
-
     def addNum(self, val):  #this function is directly copied from LC57. Insert Intervals
         """
         :type val: int
@@ -36,5 +32,30 @@ class SummaryRanges(object):
         """
         return self.memo
 
-   #BST Solution
+#Heap Solution - Heap is a binary tree, but not a Binary Search Tree
+class SummaryRanges(object):
+    def __init__(self):
+        self.intervals = []
+
+    def addNum(self, val):
+        heapq.heappush(self.intervals, (val, Interval(val, val)))
+
+    def getIntervals(self):
+        stack = []
+        while self.intervals:
+            idx, cur = heapq.heappop(self.intervals)
+            if not stack:
+                stack.append((idx, cur))
+            else:
+                prev = stack[-1][1]
+                if cur.start <= prev.end + 1:
+                    prev.end = max(prev.end, cur.end)
+                else:
+                    stack.append((idx, cur))
+        self.intervals = stack
+        heapq.heapify(self.intervals) #it still works without this line, but why?
+        return list(map(lambda x: x[1], stack))
+
+
+#BST Solution
   
