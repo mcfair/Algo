c88637266276e170ef292c05b5a19b837d3c7531
Mc Fair <mcfair18@gmail.com>
2018-10-12 10:21:28 -0700

    Update 732. My Calendar III.py

--- Interval/732. My Calendar III.py
+++ Interval/732. My Calendar III.py
@@ -1,4 +1,4 @@
-#error after passed 74/98 test cases
+#BST error after passed 74/98 test cases
 class MyCalendarThree(object):
     def __init__(self):
         self.root = None
@@ -44,3 +44,30 @@ class Node:
         self.right = None
         self.overlap = 1   
         
+        
+#Another idea is Segment Tree
+class MyCalendarThree(object):
+    def __init__(self):
+        self.st = SegTree()
+
+    def book(self, start, end):
+        self.st.add(start, end)
+        return self.st.v[1]
+    
+class SegTree(object):
+    def __init__(self):
+        self.v = collections.defaultdict(int)
+        self.lazy = collections.defaultdict(int)
+ 
+    def add(self, s, e, l = 0, r = 10**9+1, id = 1):
+        if s >= r or e <= l: return
+        if s <= l < r <= e:
+            self.lazy[id] += 1
+            self.v[id] += 1
+            return
+        m = (l + r) // 2
+        self.add(s, e, l, m, id * 2)
+        self.add(s, e, m, r, id*2+1)
+        self.v[id] = self.lazy[id] + max(self.v[id*2], self.v[id*2+1])
+            
+
