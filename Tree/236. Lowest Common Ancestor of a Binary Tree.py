fdf923e26e27268dd90973884a31cc070441c640
Mc Fair <mcfair18@gmail.com>
2018-11-14 10:10:30 -0700

    Update 236. Lowest Common Ancestor of a Binary Tree.py

--- Tree/236. Lowest Common Ancestor of a Binary Tree.py
+++ Tree/236. Lowest Common Ancestor of a Binary Tree.py
@@ -1,6 +1,5 @@
 #DFS
-class Solution(object):
-    def lowestCommonAncestor(self, root, p, q):
+def lowestCommonAncestor(self, root, p, q):
         """
         :type root: TreeNode
         :type p: TreeNode
@@ -20,3 +19,29 @@ class Solution(object):
         else:
             return left or right
             
+            
+            
+            
+def lowestCommonAncestor(self, root, p, q):
+    def path(root, goal):
+        path, stack = [], [root]
+        while True:
+            node = stack.pop()
+            if node:
+                if node not in path[-1:]:
+                    path += node,
+                    if node == goal:
+                        return path
+                    stack += node, node.right, node.left
+                else:
+                    path.pop()
+    path1 = path(root, p)
+    path2 = path(root, q)
+    LCA = None
+    for a, b in zip(path1, path2):
+        if a==b:
+            LCA = a
+        else:
+            break
+    return LCA
+        
