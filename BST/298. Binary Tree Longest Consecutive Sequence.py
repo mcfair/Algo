321ded1a334c2c4e63c5794013be54348df279a4
Leeting <mcfair18@gmail.com>
2021-08-14 10:01:10 -0700

    Update 298. Binary Tree Longest Consecutive Sequence.py

--- BST/298. Binary Tree Longest Consecutive Sequence.py
+++ BST/298. Binary Tree Longest Consecutive Sequence.py
@@ -1,11 +1,8 @@
 """
 Given the root of a binary tree, return the length of the longest consecutive sequence path.
-
 The path refers to any sequence of nodes from some starting node to any node in the tree along the parent-child connections. 
 The longest consecutive path needs to be from parent to child (cannot be the reverse).
 """
-
-
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
