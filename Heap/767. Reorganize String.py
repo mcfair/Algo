d2b1566f58de2e1200269ccc1a4c1212db1a1df0
Leeting <mcfair18@gmail.com>
2021-10-07 21:49:56 -0700

    Update 767. Reorganize String.py

--- Heap/767. Reorganize String.py
+++ Heap/767. Reorganize String.py
@@ -1,6 +1,8 @@
 ## Given a string S, check if the letters can be rearranged so that two characters that are adjacent to each other are not the same.
 ## If possible, output any possible result.  If not possible, return the empty string.
 
+https://leetcode.ca/all/767.html
+
 #Alternating outputs, using heap
 
 from collections import Counter
